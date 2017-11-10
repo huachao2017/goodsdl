d7b77bf5c4e2932d131cdbfc0c2b56b70a4a09bd
huachao2017 <21006735@qq.com>
2017-11-10 13:20:12 +0800

    - update code

--- dl/imagedetection_old.py
+++ dl/imagedetection_old.py
@@ -92,19 +92,20 @@ class ImageDetector:
         classes = np.squeeze(classes).astype(np.int32)
         scores = np.squeeze(scores)
 
-        image_dir = os.path.dirname(image_path)
-        output_image_path = os.path.join(image_dir, 'visual_' + os.path.split(image_path)[-1])
-        vis_util.visualize_boxes_and_labels_on_image_array(
-            image_np,
-            np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            self.category_index,
-            use_normalized_coordinates=True,
-            line_thickness=4)
-        output_image = Image.fromarray(image_np)
-        output_image.thumbnail((int(im_width*0.5), int(im_height*0.5)), Image.ANTIALIAS)
-        output_image.save(output_image_path)
+        if boxes.shape[0] > 0:
+            image_dir = os.path.dirname(image_path)
+            output_image_path = os.path.join(image_dir, 'visual_' + os.path.split(image_path)[-1])
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                self.category_index,
+                use_normalized_coordinates=True,
+                line_thickness=4)
+            output_image = Image.fromarray(image_np)
+            output_image.thumbnail((int(im_width*0.5), int(im_height*0.5)), Image.ANTIALIAS)
+            output_image.save(output_image_path)
 
         ret = []
         # have_classes = {}
