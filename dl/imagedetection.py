cb74132e34de46ee38270c178b4944398c2156cf
huachao2017 <21006735@qq.com>
2017-11-20 14:09:46 +0800

    - change max_num_classes = 1000

--- dl/imagedetection.py
+++ dl/imagedetection.py
@@ -61,7 +61,7 @@ class ImageDetector:
         # num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
         label_map = label_map_util.load_labelmap(self.label_path)
-        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=10000,
+        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1000,
                                                                     use_display_name=True)
         self.category_index = label_map_util.create_category_index(categories)
 
