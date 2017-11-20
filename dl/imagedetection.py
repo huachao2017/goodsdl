f04c4944e58c1edd8d84fc6d3e61aba1593fce2e
huachao2017 <21006735@qq.com>
2017-11-20 15:10:11 +0800

    - test for lock

--- dl/imagedetection.py
+++ dl/imagedetection.py
@@ -4,6 +4,7 @@ from PIL import Image
 import numpy as np
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
+import threading
 
 class ImageDetectorFactory:
     _detector = None
@@ -24,46 +25,43 @@ class ImageDetector:
         self.label_path = os.path.join(self.file_path, 'model/goods_label_map.pbtxt')
 
     def load(self):
-        #print('loading ImageDetector')
-        # self.class_to_name_dic = {1:'香海鱼豆腐',
-        #                           2:'亲嘴豆皮',
-        #                           3:'多力多玉米片',
-        #                           4:'焙朗早餐饼',
-        #                           5:'康师傅妙芙（巧克力味）',
-        #                           6:'法丽兹曲奇（抹茶慕斯）',
-        #                           7:'百奇（巧克力味）',
-        #                           8:'三元纯牛奶',
-        #                           9:'雪碧（瓶装）',
-        #                           10:'可口可乐（听装）',
-        #                           }
-        self.detection_graph = tf.Graph()
-        with self.detection_graph.as_default():
-            od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(self.model_path, 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
 
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True
-        # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 占用GPU50%的显存
-        self.session = tf.Session(graph=self.detection_graph, config=config)
+        lock = threading.Lock()
+        lock.acquire()
+        try:
+            if self.session is not None:
+                return
 
-        # Definite input and output Tensors for detection_graph
-        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-        # Each box represents a part of the image where a particular object was detected.
-        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.detection_graph = tf.Graph()
+            with self.detection_graph.as_default():
+                od_graph_def = tf.GraphDef()
+                with tf.gfile.GFile(self.model_path, 'rb') as fid:
+                    serialized_graph = fid.read()
+                    od_graph_def.ParseFromString(serialized_graph)
+                    tf.import_graph_def(od_graph_def, name='')
 
-        # num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+            config = tf.ConfigProto()
+            config.gpu_options.allow_growth = True
+            # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 占用GPU50%的显存
+            self.session = tf.Session(graph=self.detection_graph, config=config)
 
-        label_map = label_map_util.load_labelmap(self.label_path)
-        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1000,
-                                                                    use_display_name=True)
-        self.category_index = label_map_util.create_category_index(categories)
+            # Definite input and output Tensors for detection_graph
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+
+            # num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+            label_map = label_map_util.load_labelmap(self.label_path)
+            categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1000,
+                                                                        use_display_name=True)
+            self.category_index = label_map_util.create_category_index(categories)
+        finally:
+            lock.release()
 
     def detect(self,image_path,min_score_thresh=.5):
         if self.session is None:
