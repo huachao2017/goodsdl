83914c21ee694771b5dca44b621b12bde27973be
huachao2017 <21006735@qq.com>
2017-11-20 15:27:51 +0800

    - test for lock

--- dl/imagedetection.py
+++ dl/imagedetection.py
@@ -27,42 +27,40 @@ class ImageDetector:
 
     def load(self):
 
-        lock = threading.Lock()
-        lock.acquire()
+        semaphore = threading.Semaphore(1)
+        semaphore.acquire()
         try:
-            if self.category_index is not None:
-                return
+            if self.category_index is None:
+                self.detection_graph = tf.Graph()
+                with self.detection_graph.as_default():
+                    od_graph_def = tf.GraphDef()
+                    with tf.gfile.GFile(self.model_path, 'rb') as fid:
+                        serialized_graph = fid.read()
+                        od_graph_def.ParseFromString(serialized_graph)
+                        tf.import_graph_def(od_graph_def, name='')
 
-            self.detection_graph = tf.Graph()
-            with self.detection_graph.as_default():
-                od_graph_def = tf.GraphDef()
-                with tf.gfile.GFile(self.model_path, 'rb') as fid:
-                    serialized_graph = fid.read()
-                    od_graph_def.ParseFromString(serialized_graph)
-                    tf.import_graph_def(od_graph_def, name='')
+                config = tf.ConfigProto()
+                config.gpu_options.allow_growth = True
+                # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 占用GPU50%的显存
+                self.session = tf.Session(graph=self.detection_graph, config=config)
 
-            config = tf.ConfigProto()
-            config.gpu_options.allow_growth = True
-            # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 占用GPU50%的显存
-            self.session = tf.Session(graph=self.detection_graph, config=config)
+                # Definite input and output Tensors for detection_graph
+                self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+                # Each box represents a part of the image where a particular object was detected.
+                self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+                # Each score represent how level of confidence for each of the objects.
+                # Score is shown on the result image, together with the class label.
+                self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+                self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
 
-            # Definite input and output Tensors for detection_graph
-            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-            # Each box represents a part of the image where a particular object was detected.
-            self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-            # Each score represent how level of confidence for each of the objects.
-            # Score is shown on the result image, together with the class label.
-            self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-            self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+                # num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
-            # num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
-
-            label_map = label_map_util.load_labelmap(self.label_path)
-            categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1000,
-                                                                        use_display_name=True)
-            self.category_index = label_map_util.create_category_index(categories)
+                label_map = label_map_util.load_labelmap(self.label_path)
+                categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1000,
+                                                                            use_display_name=True)
+                self.category_index = label_map_util.create_category_index(categories)
         finally:
-            lock.release()
+            semaphore.release()
 
     def detect(self,image_path,min_score_thresh=.5):
         if self.category_index is None:
