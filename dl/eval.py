0031a855fa22534c1bbe3b1342f93777f2a4cc0e
huachao2017 <21006735@qq.com>
2017-11-24 19:12:17 +0800

    - setting CUDA_VISIBLE_DEVICES for train and eval

--- dl/eval.py
+++ dl/eval.py
@@ -53,7 +53,6 @@ from object_detection.builders import model_builder
 from object_detection.utils import config_util
 from object_detection.utils import label_map_util
 
-
 tf.logging.set_verbosity(tf.logging.INFO)
 
 flags = tf.app.flags
@@ -74,57 +73,59 @@ flags.DEFINE_string('input_config_path', '',
 flags.DEFINE_string('model_config_path', '',
                     'Path to a model_pb2.DetectionModel config file.')
 flags.DEFINE_boolean('run_once', False, 'Option to only run a single pass of '
-                     'evaluation. Overrides the `max_evals` parameter in the '
-                     'provided config.')
+                                        'evaluation. Overrides the `max_evals` parameter in the '
+                                        'provided config.')
 FLAGS = flags.FLAGS
 
 
 def main(unused_argv):
-  assert FLAGS.checkpoint_dir, '`checkpoint_dir` is missing.'
-  assert FLAGS.eval_dir, '`eval_dir` is missing.'
-  tf.gfile.MakeDirs(FLAGS.eval_dir)
-  if FLAGS.pipeline_config_path:
-    configs = config_util.get_configs_from_pipeline_file(
-        FLAGS.pipeline_config_path)
-    tf.gfile.Copy(FLAGS.pipeline_config_path,
-                  os.path.join(FLAGS.eval_dir, 'pipeline.config'),
-                  overwrite=True)
-  else:
-    configs = config_util.get_configs_from_multiple_files(
-        model_config_path=FLAGS.model_config_path,
-        eval_config_path=FLAGS.eval_config_path,
-        eval_input_config_path=FLAGS.input_config_path)
-    for name, config in [('model.config', FLAGS.model_config_path),
-                         ('eval.config', FLAGS.eval_config_path),
-                         ('input.config', FLAGS.input_config_path)]:
-      tf.gfile.Copy(config,
-                    os.path.join(FLAGS.eval_dir, name),
-                    overwrite=True)
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    assert FLAGS.checkpoint_dir, '`checkpoint_dir` is missing.'
+    assert FLAGS.eval_dir, '`eval_dir` is missing.'
+    tf.gfile.MakeDirs(FLAGS.eval_dir)
+    if FLAGS.pipeline_config_path:
+        configs = config_util.get_configs_from_pipeline_file(
+            FLAGS.pipeline_config_path)
+        tf.gfile.Copy(FLAGS.pipeline_config_path,
+                      os.path.join(FLAGS.eval_dir, 'pipeline.config'),
+                      overwrite=True)
+    else:
+        configs = config_util.get_configs_from_multiple_files(
+            model_config_path=FLAGS.model_config_path,
+            eval_config_path=FLAGS.eval_config_path,
+            eval_input_config_path=FLAGS.input_config_path)
+        for name, config in [('model.config', FLAGS.model_config_path),
+                             ('eval.config', FLAGS.eval_config_path),
+                             ('input.config', FLAGS.input_config_path)]:
+            tf.gfile.Copy(config,
+                          os.path.join(FLAGS.eval_dir, name),
+                          overwrite=True)
 
-  model_config = configs['model']
-  eval_config = configs['eval_config']
-  input_config = configs['eval_input_config']
+    model_config = configs['model']
+    eval_config = configs['eval_config']
+    input_config = configs['eval_input_config']
 
-  model_fn = functools.partial(
-      model_builder.build,
-      model_config=model_config,
-      is_training=False)
+    model_fn = functools.partial(
+        model_builder.build,
+        model_config=model_config,
+        is_training=False)
 
-  create_input_dict_fn = functools.partial(
-      input_reader_builder.build,
-      input_config)
+    create_input_dict_fn = functools.partial(
+        input_reader_builder.build,
+        input_config)
 
-  label_map = label_map_util.load_labelmap(input_config.label_map_path)
-  max_num_classes = max([item.id for item in label_map.item])
-  categories = label_map_util.convert_label_map_to_categories(
-      label_map, max_num_classes)
+    label_map = label_map_util.load_labelmap(input_config.label_map_path)
+    max_num_classes = max([item.id for item in label_map.item])
+    categories = label_map_util.convert_label_map_to_categories(
+        label_map, max_num_classes)
 
-  if FLAGS.run_once:
-    eval_config.max_evals = 1
+    if FLAGS.run_once:
+        eval_config.max_evals = 1
 
-  evaluator.evaluate(create_input_dict_fn, model_fn, eval_config, categories,
-                     FLAGS.checkpoint_dir, FLAGS.eval_dir)
+    evaluator.evaluate(create_input_dict_fn, model_fn, eval_config, categories,
+                       FLAGS.checkpoint_dir, FLAGS.eval_dir)
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
