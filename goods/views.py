e94275f616775c5572a2791875246da2d86ed934
huachao2017 <21006735@qq.com>
2017-11-24 18:50:10 +0800

    - train with eval at same time

--- goods/views.py
+++ goods/views.py
@@ -260,6 +260,7 @@ class ActionLogViewSet(DefaultMixin, mixins.CreateModelMixin, mixins.ListModelMi
         if serializer.instance.action == 'BT':
             # 杀死原来的train
             os.system('ps -ef | grep train.py | grep -v grep | cut -c 9-15 | xargs kill -s 9')
+            os.system('ps -ef | grep eval.py | grep -v grep | cut -c 9-15 | xargs kill -s 9')
 
             # 训练准备
             if serializer.instance.traintype == 0:
@@ -280,9 +281,19 @@ class ActionLogViewSet(DefaultMixin, mixins.CreateModelMixin, mixins.ListModelMi
             )
             logger.info(command)
             subprocess.call(command, shell=True)
+            # 评估
+            command = 'nohup python3 {}/eval.py --logtostderr --pipeline_config_path={}/faster_rcnn_nas_goods.config --checkpoint_dir={} --eval_dir={}  > /root/eval.out 2>&1 &'.format(
+                os.path.join(settings.BASE_DIR, 'dl'),
+                train_logs_dir,
+                train_logs_dir,
+                os.path.join(train_logs_dir, 'eval_log'),
+            )
+            logger.info(command)
+            subprocess.call(command, shell=True)
         elif serializer.instance.action == 'TT':
             # 杀死原来的train
             os.system('ps -ef | grep train.py | grep -v grep | cut -c 9-15 | xargs kill -s 9')
+            os.system('ps -ef | grep eval.py | grep -v grep | cut -c 9-15 | xargs kill -s 9')
 
             # 训练准备
             if serializer.instance.traintype == 0:
@@ -304,8 +315,18 @@ class ActionLogViewSet(DefaultMixin, mixins.CreateModelMixin, mixins.ListModelMi
             )
             logger.info(command)
             subprocess.call(command, shell=True)
+            # 评估
+            command = 'nohup python3 {}/eval.py --logtostderr --pipeline_config_path={}/faster_rcnn_nas_goods.config --checkpoint_dir={} --eval_dir={}  > /root/eval.out 2>&1 &'.format(
+                os.path.join(settings.BASE_DIR, 'dl'),
+                train_logs_dir,
+                train_logs_dir,
+                os.path.join(train_logs_dir, 'eval_log'),
+            )
+            logger.info(command)
+            subprocess.call(command, shell=True)
         elif serializer.instance.action == 'ST':
             os.system('ps -ef | grep train.py | grep -v grep | cut -c 9-15 | xargs kill -s 9')
+            os.system('ps -ef | grep eval.py | grep -v grep | cut -c 9-15 | xargs kill -s 9')
         elif serializer.instance.action == 'EG':
 
             lastBT = ActionLog.objects.filter(Q(action="BT") | Q(action="TT")).filter(traintype=serializer.instance.traintype).order_by('-id')[0]
