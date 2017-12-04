67a7943d0e4a38dfd345394babfe426f4b559b89
huachao2017 <21006735@qq.com>
2017-12-04 11:29:45 +0800

    - visual in line

--- dl/imagedetectionV2.py
+++ dl/imagedetectionV2.py
@@ -9,7 +9,9 @@ from object_detection.utils import label_map_util
 from .step2 import dataset
 from object_detection.utils import visualization_utils as vis_util
 import logging
+
 logger = logging.getLogger("django")
+
 
 class ImageDetectorFactory:
     _detector = {}
@@ -19,6 +21,117 @@ class ImageDetectorFactory:
         if type not in ImageDetectorFactory._detector:
             ImageDetectorFactory._detector[type] = ImageDetector(type)
         return ImageDetectorFactory._detector[type]
+
+
+def visualize_boxes_and_labels_on_image_array(image,
+                                              boxes,
+                                              classes,
+                                              scores,
+                                              labels_to_names,
+                                              instance_masks=None,
+                                              keypoints=None,
+                                              use_normalized_coordinates=False,
+                                              max_boxes_to_draw=20,
+                                              min_score_thresh=.5,
+                                              agnostic_mode=False,
+                                              line_thickness=4):
+    """Overlay labeled boxes on an image with formatted scores and label names.
+  
+    This function groups boxes that correspond to the same location
+    and creates a display string for each detection and overlays these
+    on the image. Note that this function modifies the image in place, and returns
+    that same image.
+  
+    Args:
+      image: uint8 numpy array with shape (img_height, img_width, 3)
+      boxes: a numpy array of shape [N, 4]
+      classes: a numpy array of shape [N]. Note that class indices are 1-based,
+        and match the keys in the label map.
+      scores: a numpy array of shape [N] or None.  If scores=None, then
+        this function assumes that the boxes to be plotted are groundtruth
+        boxes and plot all boxes as black with no classes or scores.
+      labels_to_names: a dict containing category dictionaries (each holding
+        category index `id` and category name `name`) keyed by category indices.
+      instance_masks: a numpy array of shape [N, image_height, image_width], can
+        be None
+      keypoints: a numpy array of shape [N, num_keypoints, 2], can
+        be None
+      use_normalized_coordinates: whether boxes is to be interpreted as
+        normalized coordinates or not.
+      max_boxes_to_draw: maximum number of boxes to visualize.  If None, draw
+        all boxes.
+      min_score_thresh: minimum score threshold for a box to be visualized
+      agnostic_mode: boolean (default: False) controlling whether to evaluate in
+        class-agnostic mode or not.  This mode will display scores but ignore
+        classes.
+      line_thickness: integer (default: 4) controlling line width of the boxes.
+  
+    Returns:
+      uint8 numpy array with shape (img_height, img_width, 3) with overlaid boxes.
+    """
+    # Create a display string (and color) for every box location, group any boxes
+    # that correspond to the same location.
+    box_to_display_str_map = vis_util.collections.defaultdict(list)
+    box_to_color_map = vis_util.collections.defaultdict(str)
+    box_to_instance_masks_map = {}
+    box_to_keypoints_map = vis_util.collections.defaultdict(list)
+    if not max_boxes_to_draw:
+        max_boxes_to_draw = boxes.shape[0]
+    for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+        if scores is None or scores[i] > min_score_thresh:
+            box = tuple(boxes[i].tolist())
+            if instance_masks is not None:
+                box_to_instance_masks_map[box] = instance_masks[i]
+            if keypoints is not None:
+                box_to_keypoints_map[box].extend(keypoints[i])
+            if scores is None:
+                box_to_color_map[box] = 'black'
+            else:
+                if not agnostic_mode:
+                    if classes[i] in labels_to_names.keys():
+                        class_name = labels_to_names[classes[i]]
+                    else:
+                        class_name = 'N/A'
+                    display_str = '{}: {}%'.format(
+                        class_name,
+                        int(100 * scores[i]))
+                else:
+                    display_str = 'score: {}%'.format(int(100 * scores[i]))
+                box_to_display_str_map[box].append(display_str)
+                if agnostic_mode:
+                    box_to_color_map[box] = 'DarkOrange'
+                else:
+                    box_to_color_map[box] = vis_util.STANDARD_COLORS[
+                        classes[i] % len(vis_util.STANDARD_COLORS)]
+
+    # Draw all boxes onto image.
+    for box, color in box_to_color_map.items():
+        ymin, xmin, ymax, xmax = box
+        if instance_masks is not None:
+            vis_util.draw_mask_on_image_array(
+                image,
+                box_to_instance_masks_map[box],
+                color=color
+            )
+            vis_util.draw_bounding_box_on_image_array(
+                image,
+                ymin,
+                xmin,
+                ymax,
+                xmax,
+                color=color,
+                thickness=line_thickness,
+                display_str_list=box_to_display_str_map[box],
+                use_normalized_coordinates=use_normalized_coordinates)
+        if keypoints is not None:
+            vis_util.draw_keypoints_on_image_array(
+                image,
+                box_to_keypoints_map[box],
+                color=color,
+                radius=line_thickness / 2,
+                use_normalized_coordinates=use_normalized_coordinates)
+
+    return image
 
 
 class ImageDetector:
@@ -74,7 +187,9 @@ class ImageDetector:
 
                 # Create the model, use the default arg scope to configure the batch norm parameters.
                 with slim.arg_scope(inception.inception_resnet_v2_arg_scope()):
-                    logits, _ = inception.inception_resnet_v2(processed_images, num_classes=len(dataset_step2.labels_to_names), is_training=False)
+                    logits, _ = inception.inception_resnet_v2(processed_images,
+                                                              num_classes=len(dataset_step2.labels_to_names),
+                                                              is_training=False)
                 probabilities = tf.nn.softmax(logits, name='detection_classes')
 
                 init_fn = slim.assign_from_checkpoint_fn(
@@ -95,9 +210,9 @@ class ImageDetector:
             #                                                             use_display_name=True)
             self.labels_to_names = dataset_step2.labels_to_names
             logger.info('end loading model')
-         # semaphore.release()
+            # semaphore.release()
 
-    def detect(self,image_path,min_score_thresh=.5):
+    def detect(self, image_path, min_score_thresh=.5):
         if self.labels_to_names is None:
             self.load()
             if self.labels_to_names is None:
@@ -129,10 +244,10 @@ class ImageDetector:
             classes.append(-1)
             if scores[i] > min_score_thresh:
                 ymin, xmin, ymax, xmax = boxes[i]
-                ymin = int(ymin*im_height)
-                xmin = int(xmin*im_width)
-                ymax = int(ymax*im_height)
-                xmax = int(xmax*im_width)
+                ymin = int(ymin * im_height)
+                xmin = int(xmin * im_width)
+                ymax = int(ymax * im_height)
+                xmax = int(xmax * im_width)
 
                 newimage = image.crop((xmin, ymin, xmax, ymax))
                 # 生成新的图片
@@ -145,12 +260,12 @@ class ImageDetector:
                 probabilities = probabilities[0]
                 sorted_inds = [i[0] for i in sorted(enumerate(-probabilities), key=lambda x: x[1])]
 
-                #print(classes[i])
-                #print(self.class_to_name_dic)
-                ret.append({'class':sorted_inds[0],
-                            'score':scores[i],
-                            'upc':self.labels_to_names[sorted_inds[0]],
-                            'xmin':xmin,'ymin':ymin,'xmax':xmax,'ymax':ymax
+                # print(classes[i])
+                # print(self.class_to_name_dic)
+                ret.append({'class': sorted_inds[0],
+                            'score': scores[i],
+                            'upc': self.labels_to_names[sorted_inds[0]],
+                            'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax
                             })
                 classes[i] = sorted_inds[0]
 
@@ -158,7 +273,7 @@ class ImageDetector:
         if boxes.shape[0] > 0:
             image_dir = os.path.dirname(image_path)
             output_image_path = os.path.join(image_dir, 'visual_' + os.path.split(image_path)[-1])
-            vis_util.visualize_boxes_and_labels_on_image_array(
+            visualize_boxes_and_labels_on_image_array(
                 image_np,
                 np.squeeze(boxes),
                 np.squeeze(classes).astype(np.int32),
