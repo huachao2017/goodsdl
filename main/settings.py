dfd5a4edbf000b3a3491603c392af1361fc0b4e4
huachao2017 <21006735@qq.com>
2017-12-14 18:50:31 +0800

    - add mysql

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,12 +81,37 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+
+    return ip
+
+my_host_ip =  get_host_ip()
+
+# TODO need remove
+if my_host_ip == '192.168.1.173':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'goodsdl',
+            'USER': 'root',
+            'PASSWORD': 'iShehui',
+            'HOST': '192.168.1.173',
+            'PORT': '3306',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- setup.sh
+++ setup.sh
@@ -1,6 +1,1 @@
 pip install -r requirements.txt
-wget http://download.tensorflow.org/models/object_detection/faster_rcnn_nas_coco_24_10_2017.tar.gz
-tar -xvf faster_rcnn_nas_coco_24_10_2017.tar.gz -C /home/src/goodsdl/train
-mv /home/src/goodsdl/train/faster_rcnn_nas_coco_24_10_2017/model.ckpt.* /home/src/goodsdl/train
-rm -f faster_rcnn_nas_coco_24_10_2017.tar.gz
-rm -rf /home/src/goodsdl/train/faster_rcnn_nas_coco_24_10_2017
