bd38fdc0ed15eecf8f2bf8501563215bee8e2ad0
huachao2017 <21006735@qq.com>
2018-01-25 18:02:44 +0800

    - update thresh

--- goods/views.py
+++ goods/views.py
@@ -95,7 +95,7 @@ class ImageViewSet(DefaultMixin, mixins.CreateModelMixin, mixins.ListModelMixin,
 
             if len(export1s) > 0 and len(export2s) > 0:
                 detector = imagedetectionV2.ImageDetectorFactory.get_static_detector(export1s[0].pk, export2s[0].pk)
-                step1_min_score_thresh = .8
+                step1_min_score_thresh = .6
                 step2_min_score_thresh = .6
                 detect_logger.info(
                     'begin detect:{},{}'.format(serializer.instance.deviceid, serializer.instance.source.path))
