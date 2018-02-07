2eac7c806c46c12d7ecbc19e23bd3665c7cd17a6
huachao2017 <21006735@qq.com>
2018-02-07 18:18:59 +0800

    - test in django

--- dl/step2/test.py
+++ dl/step2/test.py
@@ -1,6 +1,6 @@
-from dl.step2 import dataset as step2_ds
 import sys
+print(sys.path)
+from dl.step2 import dataset as step2_ds
 
 if __name__ == '__main__':
-
-    print(sys.path)
+    print('in main')
