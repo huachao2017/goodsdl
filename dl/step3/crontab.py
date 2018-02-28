b8b8b0ded2f450dee592ef589d1b19625d7029a4
huachao2017 <21006735@qq.com>
2018-02-28 17:27:16 +0800

    - urllib not support https

--- dl/step3/crontab.py
+++ dl/step3/crontab.py
@@ -26,8 +26,8 @@ def _run_train(domain, traintype):
         ('is_fineture', False),
         ('desc', 'step3 -- '+str(traintype)),
     ])
-    req = request.Request('https://{}/api/trainaction/'.format(domain))
-    req.add_header('Origin', 'https://{}/api/'.format(domain))
+    req = request.Request('http://{}/api/trainaction/'.format(domain))
+    req.add_header('Origin', 'http://{}/api/'.format(domain))
     req.add_header('User-Agent',
                    'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
     # print(req.full_url)
