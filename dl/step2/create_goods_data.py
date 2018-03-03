e3604de83195bb4765cd20167744d0238ec9b50e
huachao2017 <21006735@qq.com>
2018-03-03 14:51:42 +0800

    - fix

--- dl/step2/create_goods_data.py
+++ dl/step2/create_goods_data.py
@@ -235,7 +235,7 @@ def create_step2_goods_V2(data_dir, dataset_dir, step1_model_path, dir_day_hour=
         class_dir = os.path.join(data_dir, dirlist[i])
         if os.path.isdir(class_dir):
             if dir_day_hour is not None:
-                cur_dir_day_hour = time.strftime('%d%H', time.localtime(os.path.getctime(class_dir)))
+                cur_dir_day_hour = time.strftime('%d%H', time.localtime(os.path.getmtime(class_dir)))
                 logging.info('solve class:{}_{}'.format(dirlist[i],cur_dir_day_hour))
                 if cur_dir_day_hour != dir_day_hour:
                     continue
