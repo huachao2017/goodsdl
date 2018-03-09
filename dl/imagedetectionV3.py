2a9dd0fb97536ac97f8d0ed9aef9e6b0c9e0d29f
huachao2017 <21006735@qq.com>
2018-03-09 16:08:10 +0800

    - refactor image detection V3

--- dl/imagedetectionV3.py
+++ dl/imagedetectionV3.py
@@ -164,22 +164,141 @@ def get_labels_to_names(labels_filepath):
 
     return labels_to_names
 
+class Step1CNN:
+    def __init__(self, step1_model_dir):
+        self.model_dir = step1_model_dir
+        self.model_path = os.path.join(self.model_dir, 'frozen_inference_graph.pb')
+        self._graph = None
+        self._session = None
+
+    def load(self, config):
+        logger.info('begin loading step1 model: {}'.format(self.model_path))
+        self.graph_step1 = tf.Graph()
+        with self.graph_step1.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(self.model_path, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        logger.info('end loading step1 graph...')
+
+        # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 占用GPU50%的显存
+        self._session = tf.Session(graph=self._graph, config=config)
+
+        # Definite input and output Tensors for detection_graph
+        self._image_tensor = self._graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        self._detection_boxes = self._graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self._detection_scores = self._graph.get_tensor_by_name('detection_scores:0')
+
+    def detect(self,image_np):
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        # Actual detection.
+        (boxes, scores) = self._session.run(
+            [self._detection_boxes, self._detection_scores],
+            feed_dict={self._image_tensor: image_np_expanded})
+
+        return (boxes, scores)
+
+
+class Step2CNN:
+    def __init__(self, step2_model_dir, step2_model_name):
+        self.model_dir = step2_model_dir
+        self.model_name = step2_model_name
+        self._pre_graph = None
+        self._pre_session = None
+        self._graph = None
+        self._session = None
+        self.labels_to_names = None
+        self._isload = False
+
+    def load(self, config):
+        checkpoint = tf.train.latest_checkpoint(self.model_dir)
+        logger.info('begin loading step2 model: {}'.format(checkpoint))
+        self.labels_to_names = get_labels_to_names(os.path.join(self.model_dir, 'labels.txt'))
+        ####################
+        # Select step2 model #
+        ####################
+        network_fn = nets_factory.get_network_fn(
+            self.model_name,
+            num_classes=len(self.labels_to_names),
+            is_training=False)
+        image_size = network_fn.default_image_size
+
+        self._pre_graph = tf.Graph()
+        with self._pre_graph.as_default():
+            image_path = tf.placeholder(dtype=tf.string, name='input_image')
+            image_string = tf.read_file(image_path)
+            image = tf.image.decode_jpeg(image_string, channels=3, name='image_tensor')
+            image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+            image = tf.expand_dims(image, 0)
+            image = tf.image.resize_bilinear(image, [image_size, image_size], align_corners=False)
+            image = tf.squeeze(image, [0])
+            image = tf.subtract(image, 0.5)
+            image = tf.multiply(image, 2.0, name='output_image')
+
+        self._pre_session = tf.Session(graph=self._pre_graph, config=config)
+
+        self._input_image_tensor = self._pre_graph.get_tensor_by_name('input_image:0')
+        self._output_image_tensor = self._pre_graph.get_tensor_by_name('output_image:0')
+
+        self._graph = tf.Graph()
+        with self._graph.as_default():
+            images = tf.placeholder(dtype=tf.float32, shape=[None, image_size, image_size, 3], name='input_tensor')
+
+            # Create the model, use the default arg scope to configure the batch norm parameters.
+            logits, _ = network_fn(images)
+            probabilities = tf.nn.softmax(logits, name='detection_classes')
+
+            variables_to_restore = tf.global_variables()
+            saver = tf.train.Saver(variables_to_restore)
+
+            logger.info('end loading step2 graph...')
+
+            self._session = tf.Session(config=config)
+            saver.restore(self._session, checkpoint)
+
+        self._input_images_tensor = self._graph.get_tensor_by_name('input_tensor:0')
+        self._detection_classes = self._graph.get_tensor_by_name('detection_classes:0')
+
+
+        cluster_setting = cluster.ClusterSettings(os.path.join(self.model_dir, 'cluster.txt'))
+        self.cluster_upc_to_traintype = cluster_setting.get_main_class_name_to_traintype()
+
+        logger.info('end loading model...')
+        self._isload = True
+
+    def is_load(self):
+        return self._isload
+
+    def pre_detect(self, image_path):
+        return self._pre_session.run(self._output_image_tensor,
+                                     feed_dict={self._input_image_tensor: image_path})
+
+    def detect(self, images):
+        # 统一识别，用于加速
+        images_nps = np.array(images)
+        probabilities = self._session.run(
+            self._detection_classes, feed_dict={self._input_images_tensor: images_nps})
+        return probabilities
+
+class Step3CNN:
+    def __init__(self, traintype_to_export3ids):
+        self.traintype_to_export3ids = traintype_to_export3ids
+
+    def load(self, config):
+        pass
+
 class ImageDetector:
     def __init__(self, export1id, export2id, traintype_to_export3ids, step2_model_name):
-        self.graph_step1 = None
-        self.session_step1 = None
-        self.graph_step2 = None
-        self.session_step2 = None
-        self.labels_to_names = None
-        self.file_path, _ = os.path.split(os.path.realpath(__file__))
+        file_path, _ = os.path.split(os.path.realpath(__file__))
+        self.step1_cnn = Step1CNN(os.path.join(file_path, 'model', str(export1id)))
+        self.step2_cnn = Step2CNN(os.path.join(file_path, 'model', str(export2id)),step2_model_name)
 
-        self.step1_model_dir = os.path.join(self.file_path, 'model', str(export1id))
-        self.step2_model_dir = os.path.join(self.file_path, 'model', str(export2id))
-        self.step2_model_name = step2_model_name
-        self.step1_model_path = os.path.join(self.step1_model_dir, 'frozen_inference_graph.pb')
-        # self.step1_label_path = os.path.join(self.step1_model_dir, 'goods_label_map.pbtxt')
-
-        self.traintype_to_export3ids = traintype_to_export3ids
         self.cluster_upcs = None
         self.counter = 0
 
@@ -189,95 +308,16 @@ class ImageDetector:
             time.sleep(3)
             return
         self.counter = self.counter + 1
-        if self.labels_to_names is None:
-            logger.info('begin loading step1 model: {}'.format(self.step1_model_path))
-            self.graph_step1 = tf.Graph()
-            with self.graph_step1.as_default():
-                od_graph_def = tf.GraphDef()
-                with tf.gfile.GFile(self.step1_model_path, 'rb') as fid:
-                    serialized_graph = fid.read()
-                    od_graph_def.ParseFromString(serialized_graph)
-                    tf.import_graph_def(od_graph_def, name='')
-
-            logger.info('end loading step1 graph...')
+        if not self.step2_cnn.is_load():
             config = tf.ConfigProto()
             config.gpu_options.allow_growth = True
-            # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 占用GPU50%的显存
-            self.session_step1 = tf.Session(graph=self.graph_step1, config=config)
-
-            # Definite input and output Tensors for detection_graph
-            self.image_tensor_step1 = self.graph_step1.get_tensor_by_name('image_tensor:0')
-            # Each box represents a part of the image where a particular object was detected.
-            self.detection_boxes = self.graph_step1.get_tensor_by_name('detection_boxes:0')
-            # Each score represent how level of confidence for each of the objects.
-            # Score is shown on the result image, together with the class label.
-            self.detection_scores = self.graph_step1.get_tensor_by_name('detection_scores:0')
-            # self.detection_classes = self.graph_step1.get_tensor_by_name('detection_classes:0')
-
-            step2_checkpoint = tf.train.latest_checkpoint(self.step2_model_dir)
-            logger.info('begin loading step2 model: {}'.format(step2_checkpoint))
-            step2_labels_to_names = get_labels_to_names(os.path.join(self.step2_model_dir, 'labels.txt'))
-            ####################
-            # Select step2 model #
-            ####################
-            network_fn = nets_factory.get_network_fn(
-                self.step2_model_name,
-                num_classes=len(step2_labels_to_names),
-                is_training=False)
-            image_size = network_fn.default_image_size
-
-            self.pre_graph_step2 = tf.Graph()
-            with self.pre_graph_step2.as_default():
-                image_path = tf.placeholder(dtype=tf.string, name='input_image')
-                image_string = tf.read_file(image_path)
-                image = tf.image.decode_jpeg(image_string, channels=3, name='image_tensor')
-                image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-                image = tf.expand_dims(image, 0)
-                image = tf.image.resize_bilinear(image, [image_size, image_size], align_corners=False)
-                image = tf.squeeze(image, [0])
-                image = tf.subtract(image, 0.5)
-                image = tf.multiply(image, 2.0, name='output_image')
-
-            self.pre_sess_step2 = tf.Session(graph=self.pre_graph_step2, config=config)
-
-            self.graph_step2 = tf.Graph()
-            with self.graph_step2.as_default():
-                images = tf.placeholder(dtype=tf.float32, shape=[None, image_size, image_size, 3], name='input_tensor')
-
-                # Create the model, use the default arg scope to configure the batch norm parameters.
-                logits, _ = network_fn(images)
-                probabilities = tf.nn.softmax(logits, name='detection_classes')
-
-                variables_to_restore = tf.global_variables()
-                saver = tf.train.Saver(variables_to_restore)
-
-                logger.info('end loading step2 graph...')
-
-                config = tf.ConfigProto()
-                config.gpu_options.allow_growth = True
-                self.session_step2 = tf.Session(config=config)
-                saver.restore(self.session_step2, step2_checkpoint)
-
-            self.input_image_tensor_step2 = self.pre_graph_step2.get_tensor_by_name('input_image:0')
-            self.output_image_tensor_step2 = self.pre_graph_step2.get_tensor_by_name('output_image:0')
-            self.input_images_tensor_step2 = self.graph_step2.get_tensor_by_name('input_tensor:0')
-            # self.np_image_step2 = self.graph_step2.get_tensor_by_name('image_tensor:0')
-            self.detection_classes = self.graph_step2.get_tensor_by_name('detection_classes:0')
-
-            # label_map = label_map_util.load_labelmap(self.step1_label_path)
-            # categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1000,
-            #                                                             use_display_name=True)
-
-            cluster_setting = cluster.ClusterSettings(os.path.join(self.step2_model_dir, 'cluster.txt'))
-            self.cluster_upc_to_traintype = cluster_setting.get_main_class_name_to_traintype()
-            self.labels_to_names = step2_labels_to_names
-            logger.info('end loading model...')
-            # semaphore.release()
+            self.step1_cnn.load(config)
+            self.step2_cnn.load(config)
 
     def detect(self, image_instance, step1_min_score_thresh=.5, step2_min_score_thresh=.5):
-        if self.labels_to_names is None:
+        if not self.step2_cnn.is_load():
             self.load()
-            if self.labels_to_names is None:
+            if not self.step2_cnn.is_load():
                 logger.warning('loading model failed')
                 return None
 
@@ -292,12 +332,8 @@ class ImageDetector:
         # the array based representation of the image will be used later in order to prepare the
         # result image with boxes and labels on it.
         image_np = np.array(image)
-        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        image_np_expanded = np.expand_dims(image_np, axis=0)
-        # Actual detection.
-        (boxes, scores) = self.session_step1.run(
-            [self.detection_boxes, self.detection_scores],
-            feed_dict={self.image_tensor_step1: image_np_expanded})
+
+        (boxes, scores) = self.step1_cnn.detect(image_np)
 
         # data solving
         boxes = np.squeeze(boxes)
@@ -327,16 +363,14 @@ class ImageDetector:
                 newimage_split = os.path.split(image_path)
                 new_image_path = os.path.join(newimage_split[0], "{}_{}".format(i, newimage_split[1]))
                 newimage.save(new_image_path, 'JPEG')
-                step2_images.append(self.pre_sess_step2.run(self.output_image_tensor_step2, feed_dict={self.input_image_tensor_step2: new_image_path}))
+                step2_images.append(self.step2_cnn.pre_detect(new_image_path))
 
         if len(step2_images) <= 0:
             time2 = time.time()
             logger.info('detectV3: %s, 0, %.2f, %.2f, %.2f' % (image_instance.deviceid, time2 - time0, time1 - time0, time2 - time1))
             return None, .0
-        # 统一识别，用于加速
-        step2_images_nps = np.array(step2_images)
-        probabilities = self.session_step2.run(
-            self.detection_classes, feed_dict={self.input_images_tensor_step2: step2_images_nps})
+
+        probabilities = self.step2_cnn.detect(step2_images)
 
         labels_step2 = []
         scores_step2 = []
@@ -349,7 +383,6 @@ class ImageDetector:
             if scores_step2[i] < step2_min_score_thresh:
                 # add to database
                 self.log_problem_goods(i, image_instance, type_to_probability, sorted_inds)
-
 
         time2 = time.time()
 
@@ -366,7 +399,7 @@ class ImageDetector:
 
             class_type = labels_step2[i]
             action = 0
-            upc = self.labels_to_names[class_type]
+            upc = self.step2_cnn.labels_to_names[class_type]
             if scores_step2[i] < step2_min_score_thresh:
                 # 识别度不够
                 class_type = -1
@@ -377,7 +410,7 @@ class ImageDetector:
                 class_type = -1
                 upc = ''
                 action = 2
-            elif upc in self.cluster_upc_to_traintype:
+            elif upc in self.step2_cnn.cluster_upc_to_traintype:
                 # TODO STEP 3
                 pass
 
@@ -400,7 +433,7 @@ class ImageDetector:
                 np.array(labels_step2),
                 scores_step1,
                 scores_step2,
-                self.labels_to_names,
+                self.step2_cnn.labels_to_names,  # TODO need fix
                 use_normalized_coordinates=False,
                 step2_min_score_thresh=step2_min_score_thresh,
                 line_thickness=2)
