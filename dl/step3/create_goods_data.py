39794316f3550c16ee505bd0325a5e2a7a744833
huachao2017 <21006735@qq.com>
2018-03-12 13:48:58 +0800

    - fix bug

--- dl/step3/create_goods_data.py
+++ dl/step3/create_goods_data.py
@@ -23,18 +23,17 @@ def main(_):
         if do_traintype > 0 and traintype != do_traintype:
             continue
         output_dir = os.path.join(step3_dir,str(traintype))
-        if not tf.gfile.Exists(output_dir):
-            tf.gfile.MakeDirs(output_dir)
-            class_names = traintype_to_class_names[traintype]
-            for class_name in class_names:
-                source_dir = os.path.join(step2_dir, class_name)
-                if os.path.isdir(source_dir):
-                    if os.path.isdir(os.path.join(output_dir,class_name)):
-                        # 防止重复目录拷贝，可以增量使用
-                        print('folder exist: {}'.format(os.path.join(output_dir,class_name)))
-                        continue
-                    shutil.copytree(source_dir, os.path.join(output_dir,class_name))
-                    print('{}-->{}'.format(source_dir, output_dir))
+        tf.gfile.MakeDirs(output_dir)
+        class_names = traintype_to_class_names[traintype]
+        for class_name in class_names:
+            source_dir = os.path.join(step2_dir, class_name)
+            if os.path.isdir(source_dir):
+                if os.path.isdir(os.path.join(output_dir,class_name)):
+                    # 防止重复目录拷贝，可以增量使用
+                    # print('folder exist: {}'.format(os.path.join(output_dir,class_name)))
+                    continue
+                shutil.copytree(source_dir, os.path.join(output_dir,class_name))
+                print('{}-->{}'.format(source_dir, output_dir))
 
 if __name__ == '__main__':
     tf.app.run()
