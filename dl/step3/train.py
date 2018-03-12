f414cc9669e7c28bbeebb61808ab8478208350ee
huachao2017 <21006735@qq.com>
2018-03-12 13:53:22 +0800

    - change learning rate

--- dl/step3/train.py
+++ dl/step3/train.py
@@ -146,7 +146,7 @@ tf.app.flags.DEFINE_float(
     'learning_rate_decay_factor', 0.98, 'Learning rate decay factor.')
 
 tf.app.flags.DEFINE_float(
-    'num_epochs_per_decay', 20.0,
+    'num_epochs_per_decay', 50.0,
     'Number of epochs after which learning rate decays.')
 
 tf.app.flags.DEFINE_bool(
