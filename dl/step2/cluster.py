26d4ea57d822ed5e6c899d2b82b408c916241f76
huachao2017 <21006735@qq.com>
2018-03-12 11:20:20 +0800

    - add step3 create goods data

--- dl/step2/cluster.py
+++ dl/step2/cluster.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-import os
-import shutil
 
 class ClusterSettings:
     def __init__(self, cluster_filepath):
@@ -74,26 +72,5 @@ class ClusterSettings:
             sep = line.split(':')
             if traintype == int(sep[0]):
                 return sep[1]
-
-if __name__ == '__main__':
-    dataset_dir = '/home/src/goodsdl/media/dataset'
-    step2_dir = os.path.join(dataset_dir, 'step2')
-    step3_dir = os.path.join(dataset_dir, 'step3')
-    cluster_filepath = os.path.join(step2_dir, 'cluster.txt')
-    print(cluster_filepath)
-    cluster_settings = ClusterSettings(cluster_filepath)
-    traintype_to_class_names = cluster_settings.get_traintype_to_class_names()
-    print(traintype_to_class_names)
-
-    for traintype in traintype_to_class_names:
-        output_dir = os.path.join(step3_dir,str(traintype))
-        if not tf.gfile.Exists(output_dir):
-            tf.gfile.MakeDirs(output_dir)
-            class_names = traintype_to_class_names[traintype]
-            for class_name in class_names:
-                source_dir = os.path.join(step2_dir, class_name)
-                if os.path.isdir(source_dir):
-                    shutil.copytree(source_dir, os.path.join(output_dir,class_name))
-                    print('{}-->{}'.format(source_dir, output_dir))
 
 
---
+++ dl/step3/create_goods_data.py
@@ -0,0 +1,36 @@
+import os
+from dl.step2.cluster import ClusterSettings
+import tensorflow as tf
+import shutil
+
+tf.app.flags.DEFINE_int(
+    'traintype', 0,
+    'point traintype to create, if 0 then create all')
+FLAGS = tf.app.flags.FLAGS
+
+def main(_):
+    dataset_dir = '/home/src/goodsdl/media/dataset'
+    step2_dir = os.path.join(dataset_dir, 'step2')
+    step3_dir = os.path.join(dataset_dir, 'step3')
+    cluster_filepath = os.path.join(step2_dir, 'cluster.txt')
+    print(cluster_filepath)
+    cluster_settings = ClusterSettings(cluster_filepath)
+    traintype_to_class_names = cluster_settings.get_traintype_to_class_names()
+    print(traintype_to_class_names)
+
+    do_traintype = FLAGS.traintype
+    for traintype in traintype_to_class_names:
+        if do_traintype > 0 and traintype != do_traintype:
+            continue
+        output_dir = os.path.join(step3_dir,str(traintype))
+        if not tf.gfile.Exists(output_dir):
+            tf.gfile.MakeDirs(output_dir)
+            class_names = traintype_to_class_names[traintype]
+            for class_name in class_names:
+                source_dir = os.path.join(step2_dir, class_name)
+                if os.path.isdir(source_dir):
+                    shutil.copytree(source_dir, os.path.join(output_dir,class_name))
+                    print('{}-->{}'.format(source_dir, output_dir))
+
+if __name__ == '__main__':
+    tf.app.run()
