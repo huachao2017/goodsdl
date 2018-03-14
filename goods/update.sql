79a3c9c33131fcdbd5085641fbfe0ab7583d9cd1
huachao2017 <21006735@qq.com>
2018-03-14 13:56:08 +0800

    - update

--- goods/update.sql
+++ goods/update.sql
@@ -4,3 +4,10 @@ delete from goods_image where id < 5212223;
 OPTIMIZE TABLE `goods_goods`;
 OPTIMIZE TABLE `goods_problemgoods`;
 OPTIMIZE TABLE `goods_image`;
+
+delete from goods_goods where image_id < 7180000;
+delete from goods_problemgoods where image_id < 7180000;
+delete from goods_image where id < 7180000;
+OPTIMIZE TABLE `goods_goods`;
+OPTIMIZE TABLE `goods_problemgoods`;
+OPTIMIZE TABLE `goods_image`;
