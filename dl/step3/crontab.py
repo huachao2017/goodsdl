e0e1ffa5b0b6616cad51082b7fba9a5e4d287944
huachao2017 <21006735@qq.com>
2018-03-23 11:01:19 +0800

    - 加快循环速度

--- dl/step3/crontab.py
+++ dl/step3/crontab.py
@@ -80,26 +80,27 @@ def main(_):
             # 检查目录样本是否符合
             for i in range(10):
                 step3_dataset_dir = os.path.join(dataset_dir, 'step3', str(cur_traintype))
+                if not os.path.isdir(step3_dataset_dir):
+                    break
                 if len(os.listdir(step3_dataset_dir)) <= 1: # 必须大于两类才能分类
                     cur_traintype += 1
                 else:
                     break
 
             # 检查是否已经训练，并且训练后没有新增样本
-            do_run = True
             exports = ExportAction.objects.filter(train_action__action='T3').filter(train_action__traintype=cur_traintype).order_by('-update_time')[:1]
             if len(exports)>0:
                 step3_dataset_dir = os.path.join(dataset_dir, 'step3', str(cur_traintype))
                 filetime = datetime.datetime.fromtimestamp((os.path.getmtime(step3_dataset_dir)))
                 if filetime < exports[0].update_time:
                     logging.info('Skip train--{} because has trained in '.format(str(cur_traintype)) + time.strftime('%Y-%m-%d-%H:%M:%S', exports[0].update_time.timetuple()))
-                    do_run = False
+                    cur_traintype += 1
+                    continue
 
-            if do_run:
-                logging.info('Starting train--{} at '.format(str(cur_traintype)) + time.strftime(
-                    '%Y-%m-%d-%H:%M:%S', time.localtime()))
-                if not _run_train(domain, cur_traintype):
-                    break
+            logging.info('Starting train--{} at '.format(str(cur_traintype)) + time.strftime(
+                '%Y-%m-%d-%H:%M:%S', time.localtime()))
+            if not _run_train(domain, cur_traintype):
+                break
             cur_traintype += 1
 
         if cur_traintype > end_traintype:
