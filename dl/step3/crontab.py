3f2f7b8052a3441bbc25189aeef6feb88bc728e9
huachao2017 <21006735@qq.com>
2018-03-27 10:15:24 +0800

    - avoid mysql connection gone away

--- dl/step3/crontab.py
+++ dl/step3/crontab.py
@@ -9,7 +9,6 @@ import GPUtil as GPU
 from dl.util import get_host_ip
 import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
-django.setup()
 from goods.models import ExportAction
 
 tf.app.flags.DEFINE_string(
@@ -99,6 +98,7 @@ def main(_):
                     break
 
             # 检查是否已经训练，并且训练后没有新增样本
+            django.setup()
             exports = ExportAction.objects.filter(train_action__action='T3').filter(train_action__traintype=cur_traintype).order_by('-update_time')[:1]
             if len(exports)>0:
                 step3_dataset_dir = os.path.join(dataset_dir, common.STEP3_PREFIX+serial_postfix, str(cur_traintype))
