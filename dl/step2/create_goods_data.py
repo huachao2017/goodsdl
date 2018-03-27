c1ad887462ee9aab03af51a6cc45f985099fa85b
huachao2017 <21006735@qq.com>
2018-03-27 15:16:49 +0800

    - 用最新导出的step1进行旋转增强

--- dl/step2/create_goods_data.py
+++ dl/step2/create_goods_data.py
@@ -7,6 +7,10 @@ import time
 from PIL import Image as im
 import xml.etree.ElementTree as ET
 from dl import common
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+django.setup()
+from goods.models import ExportAction
 
 import tensorflow as tf
 
@@ -392,7 +396,8 @@ def main(_):
     dataset_dir = '/home/src/goodsdl/media/dataset'
     source_dir = os.path.join(dataset_dir, 'data_new_{}'.format(FLAGS.source_dir_serial))
     step2_dir = os.path.join(dataset_dir, common.STEP2_PREFIX if FLAGS.dest_dir_serial=='' else common.STEP2_PREFIX+'_'+FLAGS.dest_dir_serial)
-    step1_model_path = os.path.join('/home/src/goodsdl/dl/model/58/','frozen_inference_graph.pb')
+    export2s = ExportAction.objects.filter(train_action__action='T2').order_by('-update_time')[:1]
+    step1_model_path = os.path.join('/home/src/goodsdl/dl/model', str(export2s[0].pk), 'frozen_inference_graph.pb')
 
     create_step2_goods_V2(source_dir, step2_dir, step1_model_path, dir_day_hour=FLAGS.day_hour)
 
