7a43ddbb5131082669c885344bdda929b1cda743
huachao2017 <21006735@qq.com>
2018-04-09 14:35:06 +0800

    - refactor code

--- dl/step30/eval2_util.py
+++ dl/step30/eval2_util.py
@@ -9,13 +9,8 @@ import tensorflow as tf
 from preprocessing import inception_preprocessing
 
 from dl.step2.utils import visualization_utils as vis_utils
-from urllib import request, parse
-import socket
-import django
-from django.db.models import Q
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
-django.setup()
-from goods.models import TrainTask, ClusterStructure, ClusterEvalData, ClusterEvalStep, ClusterSampleScore, ClusterUpcScore
+from dl.step30 import cluster
+from goods.models import TrainTask, ClusterEvalData, ClusterEvalStep
 
 slim = tf.contrib.slim
 
@@ -283,189 +278,6 @@ def _run_checkpoint_once(task,
   sess.close()
   return (global_step, all_evaluator_metrics)
 
-def get_host_ip():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
-    finally:
-        s.close()
-
-    return ip
-
-def _run_cluster(task, precision, labels_to_names):
-    """
-    3.3.1、计算单样本聚类打分，算法：最近3次checkpoint的score，按60%，30%，10%，加权平均。（TODO：这部分可以根据map和category_ap的数据自学习）
-    3.3.2、聚类打分算法：取A->B或B->A单样本最高值作为聚类结果
-    3.3.3、聚类：设定50%为阀值进行聚类连接，记录到cluster_structure表中，修改目录存储结构，创建子训练任务
-    3.3.4、收尾：终止训练进程，当前task设为0未开始，新建一个拷贝的task，重启次数+1，修订分类总数，map清零。
-
-    :param task:
-    :param precision:
-    :return:
-    """
-    logging.info('begin cluster:{}'.format(task.pk))
-
-    # 3.3.1、计算单样本聚类打分，算法：最近3次checkpoint的score，按60%，30%，10%，加权平均。（TODO：这部分可以根据map和category_ap的数据自学习）
-    use_steps = []
-    db_steps = ClusterEvalStep.object.filter(train_task_id=task.pk).order_by('-checkpoint_step')
-    for step in db_steps:
-        if step in use_steps:
-            continue
-        use_steps.append(step)
-        if len(use_steps)==3:
-            break
-
-    sample_scores = {}
-    # tag:step:{groundtruth_label:nn,detection_label:nn,score:ff}
-    eval_datas = ClusterEvalData.objects.filter(train_task_id=task.pk).filter(Q(checkpoint_step=use_steps[0]) | Q(checkpoint_step=use_steps[1]) | Q(checkpoint_step=use_steps[2]) )
-    for eval_data in eval_datas:
-        if eval_data.sample_serial not in sample_scores:
-            sample_scores[eval_data.sample_serial] = {}
-        sample_scores[eval_data.sample_serial][eval_data.checkpoint_step] = {
-            'groundtruth_label':eval_data.groundtruth_label,
-            'detection_label': eval_data.detection_label,
-            'score': eval_data.score,
-        }
-
-    for sample_serial in sample_scores:
-        one_sample = sample_scores[sample_serial]
-        detections = {}
-        for checkpoint_step in one_sample:
-            weight = 0.0
-            if checkpoint_step == use_steps[0]:
-                weight = .6
-            elif checkpoint_step == use_steps[1]:
-                weight = .3
-            elif checkpoint_step == use_steps[2]:
-                weight = .1
-
-            if one_sample[checkpoint_step]['detection_label'] in detections:
-                detections[one_sample[checkpoint_step]['detection_label']] = detections[one_sample[checkpoint_step]['detection_label']] + weight*one_sample[checkpoint_step]['score']
-            else:
-                detections[one_sample[checkpoint_step]['detection_label']] = weight*one_sample[checkpoint_step]['score']
-
-        for detection in detections:
-            ClusterSampleScore.objects.create(
-                train_task_id=task.pk,
-                sample_serial=sample_serial,
-                upc_1=labels_to_names[one_sample['groundtruth_label']],
-                upc_2=labels_to_names[detection],
-                score=detections[detection]
-            )
-
-
-    # 3.3.2、聚类打分算法：取A->B或B->A单样本最高值作为聚类结果
-    sample_scores = ClusterSampleScore.objects.filter(train_task_id=task.pk)
-    upc_scores = {}
-    for sample_score in sample_scores:
-        if sample_score.upc_1 in upc_scores:
-            if sample_score.upc_2 in upc_scores[sample_score.upc_1]:
-                if upc_scores[sample_score.upc_1][sample_score.upc_2]<sample_score.score:
-                    upc_scores[sample_score.upc_1][sample_score.upc_2] = sample_score.score
-            else:
-                upc_scores[sample_score.upc_1][sample_score.upc_2] = sample_score.score
-        else:
-            upc_scores[sample_score.upc_1] = {sample_score.upc_2:sample_score.score}
-
-    for upc_1 in upc_scores:
-        for upc_2 in upc_scores[upc_1]:
-            # 防止重复
-            duplicate = ClusterUpcScore.objects.filter(train_task_id=task.pk).filter(upc_1=upc_2).filter(upc_2=upc_1)[:1]
-            if len(duplicate) > 0:
-                if duplicate[0].score < upc_scores[upc_1][upc_2]:
-                    duplicate[0].score = upc_scores[upc_1][upc_2]
-                    duplicate[0].save()
-            else:
-                ClusterUpcScore.objects.create(
-                    train_task_id=task.pk,
-                    upc_1=upc_1,
-                    upc_2=upc_2,
-                    score=upc_scores[upc_1][upc_2]
-                )
-
-
-    # 3.3.3.1、聚类：设定50%为阀值进行聚类连接
-    upc_scores = ClusterUpcScore.objects.filter(train_task_id=task.pk)
-    source_clusters = {} # {A:[B,C,D],B:[C,E],C:[F],E:[F],G:[A,H]}
-    for upc_score in upc_scores:
-        if upc_score.score < 0.5:
-            continue
-        if upc_score.upc_1 in source_clusters:
-            if upc_score.upc_2 in source_clusters[upc_score.upc_1]:
-                continue
-            else:
-                source_clusters[upc_score.upc_1].append(upc_score.upc_2)
-        else:
-            source_clusters[upc_score.upc_1] = [upc_score.upc_1]
-
-    sorted_cluster = {} # {A:[B,C,D,G,H],B:[C,E],C:[F],E:[F]}
-    for key in source_clusters:
-        ones = [key]
-        for one in source_clusters[key]:
-            ones.append(one)
-        sorted_ones = sorted(ones)
-        f_upc = sorted_ones[0]
-        sorted_ones.remove(f_upc)
-        if f_upc in sorted_cluster:
-            for one in sorted_ones:
-                if one not in sorted_cluster[f_upc]:
-                    sorted_cluster[f_upc].append(one)
-        else:
-            sorted_cluster[f_upc] = sorted_ones
-
-    def inner_find(f_upc,sorted_cluster):
-        ones = sorted_cluster[f_upc]
-        ret = ones.copy()
-        for one in ones:
-            finds = inner_find(one,sorted_cluster)
-            for find_one in finds:
-                if find_one not in ret:
-                    ret.append(find_one)
-
-        return ret
-
-    solved_cluster = {} # {A:[B,C,D,E,F,G,H]}
-    for f_upc in sorted_cluster:
-        solved_cluster[f_upc] = inner_find(f_upc, sorted_cluster)
-
-    # 3.3.3.2、记录到cluster_structure表中，修改目录存储结构，创建子训练任务
-    for father in solved_cluster:
-        f_structure = ClusterStructure.objects.filter(upc=father).order_by('id')[:1]
-        for upc in solved_cluster[father]:
-            c_structure = ClusterStructure.objects.filter(upc=upc).order_by('id')[:1]
-            c_structure[0].f_upc = f_structure[0].upc
-            c_structure[0].save()
-
-    # 3.3.4、收尾：终止训练进程，当前task设为0未开始，新建一个拷贝的task，重启次数+1，修订分类总数，map清零。
-
-
-def _run_export(domain, task, precision):
-    if domain is None:
-        domain = get_host_ip()
-    django.setup()
-    from goods.models import TrainAction
-    train = TrainAction.objects.filter(action='T30').filter(
-        dataset_dir=task.dataset_dir).order_by('-update_time')[:1]
-    train_data = parse.urlencode([
-        ('train_action', train.pk),
-        ('model_name', train.model_name),
-        ('precision', precision),
-    ])
-    req = request.Request('http://{}/api/exportaction/'.format(domain))
-    req.add_header('Origin', 'http://{}/api/'.format(domain))
-    req.add_header('User-Agent',
-                   'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
-    # print(req.full_url)
-    with request.urlopen(req, data=train_data.encode('utf-8')) as f:
-        print('_run_export status:', f.status, f.reason)
-        if f.status != 201:
-            raise ValueError('export error')
-        else:
-            task.state = 2
-            task.m_ap = precision
-            task.save()
-
 # TODO: Add tests.
 def repeated_checkpoint_run(train_task_id,
                             labels_to_names,
@@ -587,13 +399,13 @@ def repeated_checkpoint_run(train_task_id,
     if global_step>10000:
       if global_step>int(task.step_cnt/3) and global_step<=int(task.step_cnt/2):
         if last_precision < 0.95 and task.restart_cnt==0:
-          _run_cluster(task,last_precision,labels_to_names)
+          cluster._run_cluster(task,last_precision,labels_to_names)
       elif global_step>int(task.step_cnt/2) and global_step<=int(task.step_cnt*4/5):
         if last_precision < 0.98 and task.restart_cnt==1:
-          _run_cluster(task,last_precision,labels_to_names)
+            cluster._run_cluster(task,last_precision,labels_to_names)
       elif global_step>int(task.step_cnt4/5):
         if last_precision < 0.995 and task.restart_cnt==2:
-          _run_cluster(task,last_precision,labels_to_names)
+            cluster._run_cluster(task,last_precision,labels_to_names)
 
     if standard_cnt >= 3:
       train_ps = os.popen('ps -ef | grep train.py | grep {} | grep -v grep'.format(checkpoint_dirs[0])).readline()
@@ -602,7 +414,7 @@ def repeated_checkpoint_run(train_task_id,
         os.system('kill -s 9 {}'.format(str(pid)))
       logging.info('Finished evaluation: stardard count >= 3 and kill train process')
 
-      _run_export(export_domain, task, last_precision)
+      cluster._run_export(export_domain, task, last_precision)
       break
 
     if (max_number_of_evaluations and
@@ -613,7 +425,7 @@ def repeated_checkpoint_run(train_task_id,
     # train over then finished
     train_ps = os.popen('ps -ef | grep train.py | grep {} | grep -v grep'.format(checkpoint_dirs[0])).readline()
     if train_ps == '':
-      _run_export(export_domain, task, last_precision)
+      cluster._run_export(export_domain, task, last_precision)
       logging.info('Finished evaluation: train process was killed.')
       break
 
