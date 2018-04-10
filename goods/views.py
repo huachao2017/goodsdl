fba15c646ea7c13389ef0c897c30618eb7a77f43
huachao2017 <21006735@qq.com>
2018-04-10 17:15:44 +0800

    - fix bug

--- goods/views.py
+++ goods/views.py
@@ -729,8 +729,8 @@ class TrainActionViewSet(DefaultMixin, viewsets.ModelViewSet):
         subprocess.call(command, shell=True)
         # 评估
         command = 'nohup python3 {}/step30/eval2.py --dataset_split_name=validation --train_task_id={} --dataset_dir={} --source_dataset_dir={} --checkpoint_path={} --eval_dir={} --example_num={} --model_name={} > /root/eval30-{}.out 2>&1 &'.format(
-            task.pk,
             os.path.join(settings.BASE_DIR, 'dl'),
+            task.pk,
             train_logs_dir,
             source_dataset_dir,
             train_logs_dir,
