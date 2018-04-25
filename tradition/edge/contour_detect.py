b69ab87205b5aa420b0f7d6a41ac9af04b3e0984
huachao2017 <21006735@qq.com>
2018-04-25 11:38:57 +0800

    - refactor

---
+++ tradition/edge/contour_detect.py
@@ -0,0 +1,437 @@
+import cv2
+import numpy as np
+import os
+import shutil
+import time
+import random
+import math
+
+def _find_contour(img, image_name, output_dir=None, debug_type=0, thresh_x = 120, morphology = False, channel='all', overlapthresh=.3):
+    # param@debug_type:0,not debug; 1,store bbox file; 2,store middle caculate file; 3,show window
+    source = img.copy()
+
+    # step1: blur image
+    max_area = source.shape[0] * source.shape[1]
+    # Apply gaussian blur to the grayscale image
+    # blur = cv2.pyrMeanShiftFiltering(source, 31, 91)
+    sharpen = source
+    # blur = cv2.pyrMeanShiftFiltering(source, 21, 51)
+    # kernel_sharpen = np.array([[-1,-1,-1,-1,-1],
+    #                            [-1,2,2,2,-1],
+    #                            [-1,2,8,2,-1],
+    #                            [-2,2,2,2,-1],
+    #                            [-1,-1,-1,-1,-1]])/8.0
+    # kernel_sharpen = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    # sharpen = cv2.filter2D(sharpen, -1, kernel_sharpen)
+    if channel == 'all':
+        sharpen = cv2.cvtColor(sharpen, cv2.COLOR_BGR2GRAY)
+    else:
+        b, g, r = cv2.split(sharpen)
+        if channel == 'b':
+            sharpen = b
+        elif channel == 'g':
+            sharpen = g
+        elif channel == 'r':
+            sharpen = r
+        else:
+            sharpen = cv2.cvtColor(sharpen, cv2.COLOR_BGR2GRAY)
+
+
+    # 双向滤波比较不错
+    # blur = cv2.bilateralFilter(blur, 3, 30, 30)
+    # blur = cv2.split(blur)[0]
+    # blur = cv2.equalizeHist(blur)
+    # blur = cv2.GaussianBlur(blur, (5, 5), 0)
+    if debug_type>1:
+        sharpen_path = os.path.join(output_dir, channel+'_'+'sharpen_'+image_name)
+        cv2.imwrite(sharpen_path, sharpen)
+
+    # step2: sobel caculate edges
+    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    x = cv2.Sobel(sharpen, cv2.CV_64F, 1, 0, ksize=-1)
+    y = cv2.Sobel(sharpen, cv2.CV_64F, 0, 1, ksize=-1)
+    edges = cv2.subtract(x, y)
+    edges = cv2.convertScaleAbs(edges)
+    # absX = cv2.convertScaleAbs(x)  # 转回uint8
+    # absY = cv2.convertScaleAbs(y)
+    #
+    # edges = cv2.addWeighted(absX, 0.5, absY, 0.5, 0)
+    # edges = cv2.bilateralFilter(edges, 5, 75, 75)
+    # edges = cv2.GaussianBlur(edges, (5, 5), 0)
+    # edges = cv2.dilate(edges, kernel)
+    # edges = cv2.dilate(edges, kernel)
+    # edges = cv2.dilate(edges, kernel)
+    # edges = cv2.erode(edges, kernel)
+    # edges = cv2.erode(edges, kernel)
+    # edges = cv2.erode(edges, kernel)
+    # edges = cv2.GaussianBlur(edges, (9, 9),0)
+    if debug_type>1:
+        edges_path = os.path.join(output_dir, channel+'_'+'edges_'+image_name)
+        cv2.imwrite(edges_path, edges)
+
+    # step3: binary edges
+    _, thresh1 = cv2.threshold(edges, thresh_x, 255, cv2.THRESH_BINARY)
+    thresh2 = thresh1
+    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    # thresh2 = cv2.erode(thresh2, kernel)
+    if morphology:
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        thresh2 = cv2.morphologyEx(thresh2, cv2.MORPH_CLOSE, kernel)
+    # thresh2 = cv2.dilate(thresh2, kernel)
+    # thresh2 = cv2.dilate(thresh2, kernel)
+    # thresh2 = cv2.dilate(thresh2, kernel)
+    # thresh2 = cv2.dilate(thresh2, kernel)
+    # thresh2 = cv2.dilate(thresh2, kernel)
+    # thresh2 = cv2.erode(thresh2, kernel)
+    # thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
+    # _, thresh = cv2.threshold(gray, x, 255, cv2.THRESH_BINARY_INV)
+    # thresh = cv2.GaussianBlur(thresh, (5, 5), 0)
+    if debug_type>1:
+        thresh1_path = os.path.join(output_dir, channel+'_'+'thresh1_'+image_name)
+        cv2.imwrite(thresh1_path, thresh1)
+        if morphology:
+            thresh2_path = os.path.join(output_dir, channel+'_'+'thresh2_' + image_name)
+            cv2.imwrite(thresh2_path, thresh2)
+
+    # Find the edges
+    # edges = cv2.Canny(gray,x1,x2)
+    # edges = gray
+
+    # step4: Detect contours
+    _, contours, _ = cv2.findContours(thresh2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    print('find contours: {}'.format(len(contours)))
+    # print('first contour: {}'.format(contours[0]))
+
+    # step5: contour filter with area
+    area_to_contour = {}
+    for cnt in contours:
+        cnt = cv2.convexHull(cnt, returnPoints=True)
+        leftmost = cnt[cnt[:, :, 0].argmin()][0][0]
+        rightmost = cnt[cnt[:, :, 0].argmax()][0][0]
+        topmost = cnt[cnt[:, :, 1].argmin()][0][1]
+        bottommost = cnt[cnt[:, :, 1].argmax()][0][1]
+
+        # print('%d,%d,%d,%d' %(leftmost,rightmost,topmost,bottommost))
+        # return
+        area = (bottommost-topmost) * (rightmost-leftmost)
+        if area < max_area/100: # 去除面积过小的物体
+            continue
+        if area > max_area*.9: # 去除面积过大的物体
+            continue
+        area_to_contour[area] = cnt
+        # print(tuple(cnt[cnt[:, :, 0].argmin()][0]))
+        # print(tuple(cnt[cnt[:, :, 0].argmax()][0]))
+
+    # step6: caculate bounding box and draw contours
+    drawing_contours = np.zeros(source.shape, np.uint8)
+    areas = sorted(area_to_contour, reverse=True)
+    index = 0
+    boxes = []
+    min_rectes = []
+    for area in areas:
+        index += 1
+        # if index > top_n:
+        #     break
+        cnt = area_to_contour[area]
+        color = np.random.randint(0, 255, (3)).tolist()  # Select a random color
+        if debug_type > 1:
+            cv2.drawContours(drawing_contours, [cnt], 0, color, 1)
+        x, y, w, h = cv2.boundingRect(cnt)
+        boxes.append([x,y,x+w,y+h])
+        min_rect = cv2.minAreaRect(cnt)
+        min_rectes.append(min_rect)
+        # if debug_type > 1:
+        #     drawing_contours = cv2.rectangle(drawing_contours, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    if debug_type>1:
+        contours_path = os.path.join(output_dir, channel+'_'+'contours_'+image_name)
+        cv2.imwrite(contours_path, drawing_contours)
+
+    # step7: nms min rect
+    min_rectes = _non_max_suppression_minrect(min_rectes, .3)
+    if debug_type > 0 and len(boxes) > 0:
+        minrect = np.copy(source)
+        for min_rect in min_rectes:
+            points = cv2.boxPoints(min_rect)
+            points = np.int0(points)
+            # print(points)
+            minrect = cv2.drawContours(minrect,[points],0,(0, 0, 255),1)
+        minrect_path = os.path.join(output_dir, channel+'_'+'minrect_'+image_name)
+        cv2.imwrite(minrect_path, minrect)
+
+    # step8: nms bounding box
+    boxes = _non_max_suppression_bbox(boxes, overlapthresh)
+    if debug_type>0 and len(boxes)>0:
+        output = np.copy(source)
+        for box in boxes:
+            output = cv2.rectangle(output, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 1)
+        output_path = os.path.join(output_dir, channel+'_'+'bbox_'+image_name)
+        cv2.imwrite(output_path, output)
+
+    if debug_type>2:
+        cv2.imshow(channel+'_'+'input', sharpen)
+        cv2.imshow(channel+'_'+'edges', edges)
+        cv2.imshow(channel+'_'+'thresh1', thresh1)
+        if morphology:
+            cv2.imshow(channel+'_'+'thresh2', thresh2)
+        cv2.imshow(channel+'_'+'drawing_contours', drawing_contours)
+        if len(boxes) > 0:
+            cv2.imshow(channel+'_'+'output', output)
+
+    return boxes
+
+def _non_max_suppression_minrect(min_rectes, overlapThresh, type ='area', debug = False):
+    # if there are no boxes, return an empty list
+    return min_rectes
+    if len(min_rectes) == 0:
+        return []
+
+    boxes = np.asarray(min_rectes, dtype=np.float32)
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    # if boxes.dtype.kind == "i":
+    #     boxes = boxes.astype("float")
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    if type == 'area':
+        idxs = np.argsort(area)
+    elif type == 'y':
+        idxs = np.argsort(y2)
+    else:
+        idxs = np.argsort(x2)
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+                                               np.where(overlap > overlapThresh)[0])))
+
+    if debug:
+        print(pick)
+    # return only the bounding boxes that were picked using the
+    # integer data type
+    return boxes[pick].astype("int")
+
+def _non_max_suppression_bbox(boxes, overlapThresh, type ='area', debug = False):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+
+    boxes = np.asarray(boxes, dtype=np.float32)
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    # if boxes.dtype.kind == "i":
+    #     boxes = boxes.astype("float")
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    if type == 'area':
+        idxs = np.argsort(area)
+    elif type == 'y':
+        idxs = np.argsort(y2)
+    else:
+        idxs = np.argsort(x2)
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+                                               np.where(overlap > overlapThresh)[0])))
+
+    if debug:
+        print(pick)
+    # return only the bounding boxes that were picked using the
+    # integer data type
+    return boxes[pick].astype("int")
+
+def fish_eye_dis(img):
+    "fish eye distortion"
+    width_in = img.shape[1]
+    height_in = img.shape[0]
+    im_out = np.zeros(img.shape, np.uint8)
+    radius = max(width_in, height_in)/2
+
+    print(img.shape)
+    #assume the fov is 180
+    #R = f*theta
+    lens = radius*2/math.pi
+    for i in range(width_in):
+        for j in range(height_in):
+            #offset to center
+            x = i - width_in/2
+            y = j - height_in/2
+            r = math.sqrt(x*x + y*y)
+            theta = math.atan(r/radius)
+            if theta<0.00001:
+                k = 1
+            else:
+                k = lens*theta/r
+
+            src_x = x*k
+            src_y = y*k
+            src_x = src_x+width_in/2
+            src_y = src_y+height_in/2
+            im_out[j,i,:] = img[int(src_y),int(src_x),:]
+
+    return im_out
+
+def find_contour(input_path, area=None, output_dir=None, debug_type=0, thresh_x = 120, morphology = False, overlapthresh=.3):
+    image_dir, image_name = os.path.split(input_path)
+    if output_dir is None:
+        output_dir = image_dir
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    # step0: read image
+    img = cv2.imread(input_path)
+
+    # img = fish_eye_dis(img)
+    # output_path = os.path.join(output_dir, '_dis_'+image_name)
+    # cv2.imwrite(output_path, img)
+    #
+    # return img, [], []
+
+    if area is not None:
+        area_img = img[area[1]:area[3],area[0]:area[2],:]
+    else:
+        area_img = img
+    all_boxes = _find_contour(area_img, image_name, output_dir, debug_type=debug_type, thresh_x=thresh_x, morphology=morphology, channel='all', overlapthresh=overlapthresh)
+    r_boxes = _find_contour(area_img, image_name, output_dir, debug_type=debug_type, thresh_x=thresh_x, morphology=morphology, channel='r', overlapthresh=overlapthresh)
+    g_boxes = _find_contour(area_img, image_name, output_dir, debug_type=debug_type, thresh_x=thresh_x, morphology=morphology, channel='g', overlapthresh=overlapthresh)
+    b_boxes = _find_contour(area_img, image_name, output_dir, debug_type=debug_type, thresh_x=thresh_x, morphology=morphology, channel='b', overlapthresh=overlapthresh)
+
+    if debug_type > 0:
+        print('{},{},{},{}'.format(len(all_boxes),len(r_boxes),len(g_boxes),len(b_boxes)))
+
+    concate_boxes = np.concatenate((all_boxes,r_boxes,g_boxes,b_boxes))
+    if debug_type > 0 and len(concate_boxes)>0:
+        drawing_contours = np.zeros(img.shape, np.uint8)
+        for box in concate_boxes:
+            color = np.random.randint(0, 255, (3)).tolist()  # Select a random color
+            cv2.rectangle(drawing_contours, (box[0], box[1]), (box[2], box[3]), color, 1)
+        output_path = os.path.join(output_dir, '_contour_' + image_name)
+        cv2.imwrite(output_path, drawing_contours)
+
+    concate_boxes = _non_max_suppression_bbox(concate_boxes, overlapthresh, type='area', debug=True)
+    if area is not None:
+        concate_boxes[:, 0] = concate_boxes[:, 0] + area[0]
+        concate_boxes[:, 1] = concate_boxes[:, 1] + area[1]
+        concate_boxes[:, 2] = concate_boxes[:, 2] + area[0]
+        concate_boxes[:, 3] = concate_boxes[:, 3] + area[1]
+
+    if debug_type > 0 and len(concate_boxes)>0:
+        output = img
+        for box in concate_boxes:
+            cv2.rectangle(output, (box[0], box[1]), (box[2], box[3]), (random.randint(0,255), random.randint(0,255), random.randint(0,255)), 1)
+        output_path = os.path.join(output_dir, '_output_'+image_name)
+        cv2.imwrite(output_path, output)
+
+    scores = np.ones((len(concate_boxes)))
+    return img, concate_boxes, scores
+
+
+def _inner_find_one(image_path, output_dir, area=None, debug_type=2):
+    time0 = time.time()
+    _,boxes,_ = find_contour(image_path,  area=area, output_dir=output_dir, debug_type=debug_type, overlapthresh=.7)
+    # _,boxes,_ = find_contour(image_path, output_dir=output_dir,debug_type=1)
+    time1 = time.time()
+    print('%s:%.2f, %d' %(image_path,time1-time0, len(boxes)))
+
+if __name__ == "__main__":
+    # Enter the input image file
+    image_dir, _ = os.path.split(os.path.realpath(__file__))
+    # image_path = os.path.join(image_dir, "7_1.jpg")
+    output_dir = os.path.join(image_dir, 'contour')
+    if os.path.isdir(output_dir):
+        for image in os.listdir(output_dir):
+            tmp_path = os.path.join(output_dir, image)
+            if os.path.splitext(tmp_path)[-1] == '.jpg':
+                os.remove(tmp_path)
+
+    # for test
+    image_path = os.path.join(image_dir, "dis.jpg")
+    _inner_find_one(image_path, output_dir, area=(69,86,901,516), debug_type=2)
+
+
+    # image_path = os.path.join(image_dir, "4_1.jpg")
+    # _inner_find_one(image_path, output_dir, debug_type=2)
+    # image_path = os.path.join(image_dir, "7.jpg")
+    # _inner_find_one(image_path, output_dir, area=(69,86,901,516), debug_type=2)
+    # image_path = os.path.join(image_dir, "8.jpg")
+    # _inner_find_one(image_path, output_dir, area=(69,86,901,516), debug_type=2)
+    # image_path = os.path.join(image_dir, "9.jpg")
+    # _inner_find_one(image_path, output_dir, area=(69,86,901,516), debug_type=2)
+    # image_path = os.path.join(image_dir, "11.jpg")
+    # _inner_find_one(image_path, output_dir, area=(69,86,901,516), debug_type=2)
+
+    if cv2.waitKey(0) == 27:
+        cv2.destroyAllWindows()
---
+++ tradition/matcher/matcher.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+
+'''
+Uses SURF to match two images.
+Based on the sample code from opencv:
+  samples/python2/find_obj.py
+
+Example:
+    matcher = Matcher()
+    for i in range(8):
+        matcher.add_baseline_image(%imagepath%)
+    match_key, cnt = matcher.match_image_info(%imagepath%)
+    is_match = matcher.match_image(%imagepath%)
+
+'''
+
+import numpy
+import cv2
+import os
+
+import sys
+import time
+
+
+###############################################################################
+# Image Matching For Servicing
+###############################################################################
+
+class Matcher:
+    def __init__(self):
+        self.path_to_baseline_info = {}
+        self.upc_to_cnt = {}
+        self.detector = cv2.xfeatures2d.SURF_create(400, 5, 5)
+        self.matcher = cv2.BFMatcher(cv2.NORM_L2)
+
+    def add_baseline_image(self, image_path):
+        image = cv2.imread(image_path)
+        kp, desc = self.detector.detectAndCompute(image, None)
+        if len(kp) == 0:
+            print('error: no key point to base image:{}'.format(image_path))
+            return
+
+        upc = os.path.basename(os.path.dirname(image_path))
+        if upc in self.upc_to_cnt:
+            self.upc_to_cnt[upc] += 1
+        else:
+            self.upc_to_cnt[upc] = 1
+        key = upc + '_'+ str(self.upc_to_cnt[upc])
+        self.path_to_baseline_info[key] = (kp, desc,image)
+
+    def match_image_all_info(self, image_path, solve_size=True, match_points_cnt=4):
+        image = cv2.imread(image_path)
+        kp, desc = self.detector.detectAndCompute(image, None)
+        match_info = {}
+        if len(kp) == 0:
+            print('warn: no key point to match image:{}'.format(image_path))
+            return match_info
+
+        for key in self.path_to_baseline_info:
+            (b_kp,b_desc, b_image) = self.path_to_baseline_info[key]
+            raw_matches = self.matcher.knnMatch(b_desc, trainDescriptors=desc, k=2)  # 2
+            kp_pairs = self.filter_matches(b_kp, kp, raw_matches)
+            if len(kp_pairs) >= match_points_cnt:
+                mkp1, mkp2 = zip(*kp_pairs)
+                p1 = numpy.float32([kp.pt for kp in mkp1])
+                p2 = numpy.float32([kp.pt for kp in mkp2])
+                H, status = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)
+                if numpy.sum(status) >= match_points_cnt:
+                    if solve_size:
+                        corners = numpy.float32([[0, 0], [b_image.shape[1], 0], [b_image.shape[1], b_image.shape[0]], [0, b_image.shape[0]]])
+                        corners = numpy.int32(
+                            cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2))
+                        # area = cv2.contourArea(corners)
+                        x = corners[:, 0]
+                        y = corners[:, 1]
+                        # print(numpy.min(x),numpy.min(y),numpy.max(x),numpy.max(y))
+                        # print(image.shape[1],image.shape[0])
+                        # print(area,image.shape[1]*image.shape[0])
+
+                        # 面积接近或者（不再需要）
+                        # 四个顶点都靠近边缘或者超出边缘，则正确匹配
+                        if numpy.min(x) < image.shape[1]/5 and numpy.min(y) < image.shape[0]/5 and image.shape[1]-numpy.max(x) < image.shape[1]/5 and image.shape[0]-numpy.max(y) < image.shape[0]/5:
+                            match_info[key] = (b_image,image,kp_pairs,status,H)
+                    else:
+                        match_info[key] = (b_image,image,kp_pairs,status,H)
+        return match_info
+
+    def filter_matches(self, kp1, kp2, matches, ratio=0.75):
+        mkp1, mkp2 = [], []
+        for m in matches:
+            if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+                m = m[0]
+                mkp1.append(kp1[m.queryIdx])
+                mkp2.append(kp2[m.trainIdx])
+        kp_pairs = list(zip(mkp1, mkp2))
+        return kp_pairs
+
+    def match_image_top_n(self, image_path, n=5, solve_size=True, match_points_cnt=5, visual=True):
+        match_info = self.match_image_all_info(image_path, solve_size=solve_size, match_points_cnt=match_points_cnt)
+        if len(match_info) == 0:
+            return None,0
+        sorted_match_info = sorted(match_info.items(), key=lambda d: numpy.sum(d[1][3]), reverse=True)
+        top_n = sorted_match_info[:n]
+        ret = []
+        for match in top_n:
+            ret.append((match[0],numpy.sum(match[1][3])))
+        if visual:
+            for i in range(len(top_n)):
+                match = top_n[i]
+                visual_path = os.path.join(os.path.dirname(image_path),'visual_{}_{}_{}'.format(match[0],i,os.path.basename(image_path)) )
+                self.match_visual(visual_path, match[1][0],match[1][1],match[1][2],match[1][3],match[1][4])
+        return ret
+
+    def match_image_best_one(self, image_path, solve_size=True, match_points_cnt=5, visual=True):
+        match_info = self.match_image_all_info(image_path, solve_size=solve_size, match_points_cnt=match_points_cnt)
+        if len(match_info) == 0:
+            return None,0
+        sorted_match_info = sorted(match_info.items(), key=lambda d: numpy.sum(d[1][3]), reverse=True)
+        best_match = sorted_match_info[0]
+        ret = (best_match[0],numpy.sum(best_match[1][3]))
+        if visual:
+            visual_path = os.path.join(os.path.dirname(image_path),'visual_{}_{}'.format(best_match[0],os.path.basename(image_path)) )
+            self.match_visual(visual_path, best_match[1][0],best_match[1][1],best_match[1][2],best_match[1][3],best_match[1][4])
+        return ret
+
+    def is_find_match(self, image_path, solve_size=True, match_points_cnt=5, visual=True):
+        key, cnt = self.match_image_best_one(image_path, solve_size=solve_size, match_points_cnt=match_points_cnt, visual=visual)
+        return key != None
+
+    def match_visual(self, visual_path, img1, img2, kp_pairs, status=None, H=None):
+        h1, w1 = img1.shape[:2]
+        h2, w2 = img2.shape[:2]
+        vis = numpy.zeros((max(h1, h2), w1 + w2, 3), numpy.uint8)
+        vis[:h1, :w1, :] = img1
+        vis[:h2, w1:w1 + w2, :] = img2
+        # vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
+
+        if H is not None:
+            corners = numpy.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
+            corners = numpy.int32(cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0))
+            cv2.polylines(vis, [corners], True, (255, 255, 255))
+            # center = numpy.int32(numpy.sum(corners, 0) / len(corners))
+            # print(center)
+            # col = (255, 0, 0)
+            # r = 2
+            # thickness = 3
+            # cv2.line(vis, (center[0] - r, center[1] - r), (center[0] + r, center[1] + r), col, thickness)
+            # cv2.line(vis, (center[0] - r, center[1] + r), (center[0] + r, center[1] - r), col, thickness)
+
+        if status is None:
+            status = numpy.ones(len(kp_pairs), numpy.bool_)
+        p1 = numpy.int32([kpp[0].pt for kpp in kp_pairs])
+        p2 = numpy.int32([kpp[1].pt for kpp in kp_pairs]) + (w1, 0)
+
+        green = (0, 255, 0)
+        red = (0, 0, 255)
+        white = (255, 255, 255)
+        kp_color = (51, 103, 236)
+        for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+            if inlier:
+                col = green
+                cv2.circle(vis, (x1, y1), 2, col, -1)
+                cv2.circle(vis, (x2, y2), 2, col, -1)
+            else:
+                col = red
+                r = 2
+                thickness = 3
+                cv2.line(vis, (x1 - r, y1 - r), (x1 + r, y1 + r), col, thickness)
+                cv2.line(vis, (x1 - r, y1 + r), (x1 + r, y1 - r), col, thickness)
+                cv2.line(vis, (x2 - r, y2 - r), (x2 + r, y2 + r), col, thickness)
+                cv2.line(vis, (x2 - r, y2 + r), (x2 + r, y2 - r), col, thickness)
+        vis0 = vis.copy()
+        for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
+            if inlier:
+                cv2.line(vis, (x1, y1), (x2, y2), green)
+
+        cv2.imwrite(visual_path, vis)
+
+
+###############################################################################
+# Test Main
+###############################################################################
+
+def test_1():
+    time0 = time.time()
+    matcher = Matcher()
+    time1 = time.time()
+    for i in range(8):
+        matcher.add_baseline_image('images/%d.jpg' % (i+1))
+    time2 = time.time()
+    match_key, cnt = matcher.match_image_best_one('images/9.jpg')
+    time3 = time.time()
+    print('MATCH: %.2f, %.2f, %.2f, %.2f' % (time3 - time0, time1 - time0, time2 - time1, time3 - time2))
+    print(match_key, cnt)
+
+def test_2(image1,image2):
+    time0 = time.time()
+    matcher = Matcher()
+    time1 = time.time()
+    matcher.add_baseline_image(image1)
+    time2 = time.time()
+    match_key, cnt = matcher.match_image_best_one(image2)
+    time3 = time.time()
+    print('MATCH: %.2f, %.2f, %.2f, %.2f' % (time3 - time0, time1 - time0, time2 - time1, time3 - time2))
+    print(match_key, cnt)
+
+def test_3(image1,image2):
+    time0 = time.time()
+    matcher = Matcher()
+    time1 = time.time()
+    matcher.add_baseline_image(image1)
+    time2 = time.time()
+    match_key, cnt = matcher.match_image_best_one(image2, match_points_cnt=0, solve_size=False)
+    time3 = time.time()
+    print('MATCH: %.2f, %.2f, %.2f, %.2f' % (time3 - time0, time1 - time0, time2 - time1, time3 - time2))
+    print(match_key, cnt)
+
+if __name__ == '__main__':
+    """Test code: Uses the two specified"""
+
+    # test_1()
+    # sys.exit(0)
+    fn1 = 'images/noodles/1.jpg'
+    fn2 = 'images/noodles/2.jpg'
+    test_3(fn1, fn2)
