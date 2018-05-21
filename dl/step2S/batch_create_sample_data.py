7b43c1a02ebb37bc7f74270e862d549705eebb48
huachao2017 <21006735@qq.com>
2018-05-21 16:52:31 +0800

    - fix bug

--- dl/step2S/batch_create_sample_data.py
+++ dl/step2S/batch_create_sample_data.py
@@ -3,10 +3,8 @@ import os
 
 import cv2
 import django
-import numpy as np
-
+import shutil
 from dl import common
-from tradition.matcher.matcher import Matcher
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 django.setup()
@@ -20,148 +18,34 @@ import tensorflow as tf
 
 def solves_one_class(class_dir,
                      class_name,
-                     output_class_dir,
-                     session_step1,
-                     image_tensor_step1,
-                     detection_boxes,
-                     detection_scores
+                     output_class_dir
                      ):
     if class_name == 'ziptop-drink-stand' or class_name == 'bottled-drink-stand':
         return 0
     sample_cnt = 0
-    matcher = None
     filelist = os.listdir(class_dir)
     for j in range(0, len(filelist)):
         image_path = os.path.join(class_dir, filelist[j])
         prefix = filelist[j].split('_')[0]
-        example, ext = os.path.splitext(image_path)
-        if ext != ".jpg" or prefix == 'visual':
+        postfix = filelist[j].split('_')[-1]
+        if prefix == 'visual' or postfix != 'augment0.jpg':
             continue
 
         logging.info('solve image:{}'.format(image_path))
-        img = cv2.imread(image_path)
 
         output_image_path = os.path.join(output_class_dir, os.path.basename(image_path))
-        # if tf.gfile.Exists(output_image_path):
-        #     # 文件存在不再重新生成，从而支持增量生成
-        #     continue
+        shutil.copy(image_path,output_image_path)
 
-        im_height = img.shape[0]
-        im_width = img.shape[1]
-        image_np = np.asarray(img).reshape(
-            (im_height, im_width, 3)).astype(np.uint8)
-        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        image_np_expanded = np.expand_dims(image_np, axis=0)
-        # Actual detection.
-        # logging.info("begin detect...")
-        (boxes, scores) = session_step1.run(
-            [detection_boxes, detection_scores],
-            feed_dict={image_tensor_step1: image_np_expanded})
-        # logging.info("end detect...")
-        # data solving
-        boxes = np.squeeze(boxes)
-        # classes = np.squeeze(classes).astype(np.int32)
-        scores_step1 = np.squeeze(scores)
-
-        new_boxes = []
-        is_sample = True
-        for j in range(boxes.shape[0]):
-            if scores_step1[j] > 0.7:
-                new_boxes.append(boxes[j])
-        if len(new_boxes) <= 0:
-            logging.error("not detected error! image:{}.".format(image_path))
-            continue
-        elif len(new_boxes) == 1:
-            ymin, xmin, ymax, xmax = new_boxes[0]
-            ymin = int(ymin * im_height)
-            xmin = int(xmin * im_width)
-            ymax = int(ymax * im_height)
-            xmax = int(xmax * im_width)
-
-            newimage = img[ymin:ymax, xmin:xmax]
-            cv2.imwrite(output_image_path, newimage)
-            if matcher is None:
-                matcher = Matcher(debug=True, visual=True)
-                if not matcher.add_baseline_image(output_image_path, class_name):
-                    is_sample = False
-            else:
-                if matcher.is_find_match(output_image_path):
-                    is_sample = False
-                else:
-                    if not matcher.add_baseline_image(output_image_path, class_name):
-                        is_sample = False
-        else:
-            index = -1
-            area = 0
-            filter_area = im_height * im_width * .9
-            for j in range(len(new_boxes)):
-                # 取最大面积的识别物体
-                ymin, xmin, ymax, xmax = new_boxes[j]
-                ymin = int(ymin * im_height)
-                xmin = int(xmin * im_width)
-                ymax = int(ymax * im_height)
-                xmax = int(xmax * im_width)
-                if area < (ymax - ymin) * (xmax - xmin) and filter_area > (ymax - ymin) * (xmax - xmin):
-                    area = (ymax - ymin) * (xmax - xmin)
-                    index = j
-
-            if index >= 0:
-                ymin, xmin, ymax, xmax = new_boxes[index]
-                ymin = int(ymin * im_height)
-                xmin = int(xmin * im_width)
-                ymax = int(ymax * im_height)
-                xmax = int(xmax * im_width)
-                # augment_newimage = augment_image.crop((xmin, ymin, xmax, ymax))
-                newimage = img[ymin:ymax, xmin:xmax]
-                cv2.imwrite(output_image_path, newimage)
-                if matcher is None:
-                    matcher = Matcher(debug=True)
-                    if not matcher.add_baseline_image(output_image_path, class_name):
-                        is_sample = False
-                else:
-                    if matcher.is_find_match(output_image_path):
-                        is_sample = False
-                    else:
-                        if not matcher.add_baseline_image(output_image_path, class_name):
-                            is_sample = False
-            else:
-                logging.error("not detected error! image:{}.".format(image_path))
-                continue
-
-
-        if is_sample:
-            SampleImageClass.objects.create(
-                source='{}/{}/{}/{}'.format(settings.DATASET_DIR_NAME, common.SAMPLE_PREFIX+'_'+common.STEP2S_PREFIX, class_name,os.path.basename(output_image_path)),
-                deviceid=common.STEP2S_PREFIX,
-                upc=class_name,
-                name=class_name,
-            )
-            sample_cnt += 1
-        else:
-            os.remove(output_image_path)
+        SampleImageClass.objects.create(
+            source='{}/{}/{}/{}'.format(settings.DATASET_DIR_NAME, common.SAMPLE_PREFIX+'_'+common.STEP2S_PREFIX, class_name,os.path.basename(output_image_path)),
+            deviceid=common.STEP2S_PREFIX,
+            upc=class_name,
+            name=class_name,
+        )
+        sample_cnt += 1
     return sample_cnt
 
 def create_sample(data_dir, output_dir, step1_model_path):
-    graph_step1 = tf.Graph()
-    with graph_step1.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(step1_model_path, 'rb') as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')
-
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    # config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 占用GPU50%的显存
-    session_step1 = tf.Session(graph=graph_step1, config=config)
-
-    # Definite input and output Tensors for detection_graph
-    image_tensor_step1 = graph_step1.get_tensor_by_name('image_tensor:0')
-    # Each box represents a part of the image where a particular object was detected.
-    detection_boxes = graph_step1.get_tensor_by_name('detection_boxes:0')
-    # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
-    detection_scores = graph_step1.get_tensor_by_name('detection_scores:0')
 
     # class_names = get_class_names(os.path.join(os.path.dirname(step1_model_path), dataset_utils.LABELS_FILENAME))
     """返回所有图片文件路径"""
@@ -186,14 +70,9 @@ def create_sample(data_dir, output_dir, step1_model_path):
                 class_dir,
                 class_name,
                 output_class_dir,
-                session_step1,
-                image_tensor_step1,
-                detection_boxes,
-                detection_scores,
             )
 
     logging.info("sample create complete: {}".format(sample_total))
-    session_step1.close()
 
 tf.app.flags.DEFINE_string(
     'device', "0",
