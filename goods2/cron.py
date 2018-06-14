363c7aa1d868d97ce2f0fd80469486ebb49468cf
huachao2017 <21006735@qq.com>
2018-06-14 19:18:33 +0800

    - test

--- goods2/cron.py
+++ goods2/cron.py
@@ -1,6 +1,10 @@
 import logging
+import os
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+django.setup()
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('django')
 
 def test():
     logger.info('test cron')
