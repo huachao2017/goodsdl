9a0e0e1eab671ef3cc53af7ec685911d235fc73a
huachao2017 <21006735@qq.com>
2018-06-15 15:32:06 +0800

    - test

--- goods2/cron.py
+++ goods2/cron.py
@@ -1,8 +1,8 @@
 import logging
 import os
-# import django
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
-# django.setup()
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+django.setup()
 
 logger = logging.getLogger('')
 
