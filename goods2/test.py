2288530cde3752ef67f0a1a4b35296ef3fcea44f
huachao2017 <21006735@qq.com>
2018-06-21 16:11:33 +0800

    - test

--- goods2/test.py
+++ goods2/test.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.test import Client
 from goods2.models import TaskLog
 
 class ImageTestCase(TestCase):
@@ -10,7 +11,7 @@ class ImageTestCase(TestCase):
         task_log_qs = TaskLog.objects.filter(state=1)
         self.assertEqual(len(task_log_qs), 1)
 
-    def test_tasklog_create2(self):
-        task_log = TaskLog.objects.create(name='test', ip='test', message='')
-        task_log_qs = TaskLog.objects.filter(state=1)
-        self.assertEqual(len(task_log_qs), 1)
+    def test_image_get(self):
+        c = Client()
+        response = c.get('/api2/image')
+        self.assertEqual(response.status_code,200)
