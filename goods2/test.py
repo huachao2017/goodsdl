37b5850921fbaa3cba716425cbe18fd5cf44f077
huachao2017 <21006735@qq.com>
2018-06-21 19:23:34 +0800

    - test

--- goods2/test.py
+++ goods2/test.py
@@ -1,8 +1,11 @@
 from django.test import TestCase
-from django.test import Client
 from goods2.models import TaskLog
+from io import BytesIO
+
 
 class TaskLogTestCase(TestCase):
+    reset_sequences = True
+
     def setUp(self):
         pass
 
@@ -14,7 +17,13 @@ class TaskLogTestCase(TestCase):
     def test_tasklog_get(self):
         task_log = TaskLog.objects.create(name='test', ip='test', message='')
         response = self.client.get('/api2/tasklog/')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         task_log_list = response.data['results']
         self.assertEqual(len(task_log_list), 1)
         self.assertEqual(task_log_list[0]['name'], 'test')
+
+    def test_image_create(self):
+        img = BytesIO(b'mybinarydata')
+        img.name = 'myimage.jpg'
+        response = self.client.post('api2/image/', {'deviceid': '0', 'identify': '1111', 'source': img})
+        self.assertEqual(response.status_code, 201)
