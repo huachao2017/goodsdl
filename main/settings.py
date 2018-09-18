f1f9183e8595a997d735d76e12a6c1909959a4b8
huachao2017 <21006735@qq.com>
2018-09-18 16:58:49 +0800

    - 增加当前页数

--- main/settings.py
+++ main/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'main.urls'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'main.pagination.MyPageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
