e91299dbb076a60d3bf95325b100b87516b42119
huachao2017 <21006735@qq.com>
2018-09-28 15:17:57 +0800

    - 测试深度图

--- tradition/edge/test.py
+++ tradition/edge/test.py
@@ -6,15 +6,34 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 
+def pooling(feature_map, size=2, stride=2):
+  # Preparing the output of the pooling operation.
+  pool_out = np.zeros((np.uint16((feature_map.shape[0] - size + 1) / stride),
+                          np.uint16((feature_map.shape[1] - size + 1) / stride),
+                          feature_map.shape[-1]))
+  for map_num in range(feature_map.shape[-1]):
+    r2 = 0
+    for r in np.arange(0, feature_map.shape[0] - size - 1, stride):
+      c2 = 0
+      for c in np.arange(0, feature_map.shape[1] - size - 1, stride):
+        pool_out[r2, c2, map_num] = np.max([feature_map[r:r + size, c:c + size]])
+        c2 = c2 + 1
+      r2 = r2 + 1
+  return pool_out
 
 base_dir, _ = os.path.split(os.path.realpath(__file__))
 image_dir = os.path.join(base_dir, 'images')
-depth_path = os.path.join(image_dir, "03_d.png")
+depth_path = os.path.join(image_dir, "04_d.png")
 
 img = cv2.imread(depth_path, cv2.IMREAD_UNCHANGED)#, flags=cv2.COLOR_BGR2GRAY)
 # print(img.shape)
 # print(img)
 depth = img[:,:,0]+img[:,:,1]*256+img[:,:,2]*256*256
+depth = np.expand_dims(depth,2)
+depth = pooling(depth,2,2)
+depth = pooling(depth,2,2)
+depth = pooling(depth,2,2)
+depth = depth[:,:,0]
 print(depth.shape)
 # print(a.shape)
 # print(a)
