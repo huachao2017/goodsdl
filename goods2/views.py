ecae255091efcc9986c1558a22c45e9a17aaa784
huachao2017 <21006735@qq.com>
2018-10-18 15:44:57 +0800

    - test

--- goods2/views.py
+++ goods2/views.py
@@ -88,6 +88,18 @@ class UserImageViewSet(DefaultMixin, mixins.ListModelMixin, mixins.RetrieveModel
         )
         return Response(util.wrap_ret([]), status=status.HTTP_200_OK)
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        print(serializer.data)
+        return Response(serializer.data)
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         try:
