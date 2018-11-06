3b2ddb7a422238cfe194144e5f3e2ab5fff540b0
huachao2017 <21006735@qq.com>
2018-11-06 14:44:25 +0800

    - 修订ip

--- dl/test/test_verify.py
+++ dl/test/test_verify.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 import urllib
 
 def main(_):
-    picurl = 'http://192.168.1.172/media/images/275/20180503/105652_105652.409527_ai.jpg'
+    picurl = 'http://192.168.1.62/media/images/275/20180503/105652_105652.409527_ai.jpg'
     picurl = urllib.parse.urlencode({'picurl':picurl})
     print(picurl)
-    url = 'http://192.168.1.170/api/verifycnt?deviceid=0&paymentID=1&{}&goodscnt=3'.format(picurl)
+    url = 'http://192.168.1.60/api/verifycnt?deviceid=0&paymentID=1&{}&goodscnt=3'.format(picurl)
     print(url)
     response = urllib.request.urlopen(url)
 
