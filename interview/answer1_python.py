d5f84a6828273c8e880ea2f8e17128d18706b4b6
huachao2017 <21006735@qq.com>
2019-05-09 18:08:56 +0800

    - 修订

--- interview/answer1_python.py
+++ interview/answer1_python.py
@@ -2,8 +2,6 @@
 用python编程：有一个4*4的二维矩阵，请按如下图顺序输出里面的值。
 """
 
-import numpy as np
-
 input = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 
 def output_edge(input, output,direction = 0):
@@ -28,7 +26,6 @@ def output_edge(input, output,direction = 0):
     output_edge(input,output,3)
   elif direction == 3:
     row = len(input)
-    column = len(input[0])
     output.append([input[row-i-1][0] for i in range(row)])
     for i in range(row):
       input[i].pop(0)
