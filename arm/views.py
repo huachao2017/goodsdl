dba79611c78b21b8ee57cafa0a60b36eba43c383
huachao2017 <21006735@qq.com>
2019-05-22 10:37:15 +0800

    - 修订机械臂商品用长宽匹配

--- arm/views.py
+++ arm/views.py
@@ -23,6 +23,7 @@ from arm.dl import imagedetection
 from goods2 import common as goods2_common
 from arm import common
 import datetime
+import math
 
 
 # Create your views here.
@@ -66,18 +67,24 @@ class ArmImageViewSet(DefaultMixin, mixins.CreateModelMixin, mixins.ListModelMix
         tmp_dir = None
         if len(min_rectes)>0:
             # 准备阶段
-            last_normal_train_qs = TrainAction.objects.filter(state=goods2_common.TRAIN_STATE_COMPLETE).filter(
-                deviceid='100000').exclude(action='TC').order_by('-id')
-            if len(last_normal_train_qs) > 0:
-                last_train = last_normal_train_qs[0]
-                last_normal_train_model = \
-                TrainModel.objects.filter(train_action_id=last_train.pk).exclude(model_path='').order_by('-id')[0]
-                detector = imagedetection.ImageDetectorFactory.get_static_detector(
-                    last_normal_train_model)
-                image = Image.open(serializer.instance.rgb_source.path)
-                tmp_dir = '{}/tmp'.format(os.path.dirname(serializer.instance.rgb_source.path))
-                if not tf.gfile.Exists(tmp_dir):
-                    tf.gfile.MakeDirs(tmp_dir)
+            detector = {
+                '1':[66,69],
+                '3':[62,103],
+                '7':[150,227],
+                '8':[165,167],
+            }
+            # last_normal_train_qs = TrainAction.objects.filter(state=goods2_common.TRAIN_STATE_COMPLETE).filter(
+            #     deviceid='100000').exclude(action='TC').order_by('-id')
+            # if len(last_normal_train_qs) > 0:
+            #     last_train = last_normal_train_qs[0]
+            #     last_normal_train_model = \
+            #     TrainModel.objects.filter(train_action_id=last_train.pk).exclude(model_path='').order_by('-id')[0]
+            #     detector = imagedetection.ImageDetectorFactory.get_static_detector(
+            #         last_normal_train_model)
+            #     image = Image.open(serializer.instance.rgb_source.path)
+            #     tmp_dir = '{}/tmp'.format(os.path.dirname(serializer.instance.rgb_source.path))
+            #     if not tf.gfile.Exists(tmp_dir):
+            #         tf.gfile.MakeDirs(tmp_dir)
         else:
             deleted_dir = '{}/deleted'.format(os.path.dirname(serializer.instance.rgb_source.path))
             if not tf.gfile.Exists(deleted_dir):
@@ -93,15 +100,33 @@ class ArmImageViewSet(DefaultMixin, mixins.CreateModelMixin, mixins.ListModelMix
             # 检测类型
             upcs = [0,]
             scores = [0,]
+
+            min_upc = 'none'
+            min_distance = 1000000
             if detector is not None:
-                oneimage = image.crop((boxes[index][0], boxes[index][1], boxes[index][2], boxes[index][3]))
-                one_image_path = os.path.join(tmp_dir,'%d_%d.jpg' % (serializer.instance.pk, index))
-                oneimage.save(one_image_path, 'JPEG')
-                upcs, scores = detector.detect(one_image_path)
-                shutil.move(one_image_path,os.path.join(tmp_dir,'%d_%d_%s_%.2f.jpg' % (serializer.instance.pk, index, upcs[0], scores[0])))
+                w = min_rect[1][0]
+                h = min_rect[1][1]
+                if w > h:
+                    w = min_rect[1][1]
+                    h = min_rect[1][0]
+                for upc in detector:
+                    upc_w = detector[upc][0]
+                    upc_h = detector[upc][1]
+                    distance = math.sqrt((w-upc_w)*(w-upc_w) + (h-upc_h)*(h-upc_h))
+                    if distance < min_distance:
+                        min_upc = upc
+                        min_distance = distance
+            # if detector is not None:
+            #     oneimage = image.crop((boxes[index][0], boxes[index][1], boxes[index][2], boxes[index][3]))
+            #     one_image_path = os.path.join(tmp_dir,'%d_%d.jpg' % (serializer.instance.pk, index))
+            #     oneimage.save(one_image_path, 'JPEG')
+            #     upcs, scores = detector.detect(one_image_path)
+            #     shutil.move(one_image_path,os.path.join(tmp_dir,'%d_%d_%s_%.2f.jpg' % (serializer.instance.pk, index, upcs[0], scores[0])))
+
+
             logger.info('center: %d,%d; w*h:%d,%d; theta:%d; z:%d, boxes: x1:%d, y1:%d, x2:%d, y2:%d, type:%s, score:%.2f' % (
             min_rect[0][0], min_rect[0][1], min_rect[1][0], min_rect[1][1], min_rect[2], z[index], boxes[index][0],
-            boxes[index][1], boxes[index][2], boxes[index][3], upcs[0], scores[0]))
+            boxes[index][1], boxes[index][2], boxes[index][3], min_upc, min_distance))
             one = {
                 'x': min_rect[0][0],
                 'y': min_rect[0][1],
@@ -115,7 +140,8 @@ class ArmImageViewSet(DefaultMixin, mixins.CreateModelMixin, mixins.ListModelMix
                     'xmax': boxes[index][2],
                     'ymax': boxes[index][3],
                 },
-                'upc': upcs[0],
+                'upc': min_upc,
+                'distance': min_distance
             }
             ret.append(one)
             index += 1
