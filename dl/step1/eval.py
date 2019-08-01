1543d1127338e34dcc1271a5603870254839b73c
huachao2017 <21006735@qq.com>
2019-08-01 14:46:28 +0800

    - fix

--- dl/step1/eval.py
+++ dl/step1/eval.py
@@ -81,7 +81,7 @@ FLAGS = flags.FLAGS
 
 def main(unused_argv):
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
     tf.logging.set_verbosity(tf.logging.INFO)
     logger = logging.getLogger()
     logger.setLevel('INFO')
