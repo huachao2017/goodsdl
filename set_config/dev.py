b6aed8ec658cbf2b151faa482efd42443a899ad3
LRJliurj <908601417@qq.com>
2019-08-20 11:51:20 +0800

    修改阈值0.05----0.1

--- set_config/dev.py
+++ set_config/dev.py
@@ -4,7 +4,7 @@ yolov3_params={
     'anchors_path' :'./goods/freezer/keras_yolo3/model_data/yolo_anchors.txt',
     'classes_path' : './goods/freezer/keras_yolo3/model_data/voc_classes.txt',
     'label_path':'./goods/freezer/keras_yolo3/model_data/goods_label_map.pbtxt',
-    'score' :0.05,
+    'score' :0.1,
     'iou' :0.45,
     'model_image_size' : (416, 416),
     'gpu_num' : 1,
