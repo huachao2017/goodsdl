c08a2d13deff5f61d432bda40e48418f6cffc720
huachao2017 <21006735@qq.com>
2019-09-05 11:51:15 +0800

    - 修订冰柜图片生成方式

--- goods/models.py
+++ goods/models.py
@@ -42,10 +42,16 @@ class Image(models.Model):
     create_time = models.DateTimeField('date created', auto_now_add=True,db_index=True)
 
 
+def freezer_image_upload_source(instance, filename):
+    now = datetime.datetime.now()
+    return '{}/{}/{}/{}/{}_{}_{}'.format(settings.DETECT_DIR_NAME, 'freezer', now.strftime('%Y%m'),
+                                         now.strftime('%d%H'), now.strftime('%M%S'), str(now.time()), filename)
+
+
 class FreezerImage(models.Model):
     deviceid = models.CharField(max_length=20, default='0', db_index=True)
     ret = models.TextField(default='')
-    source = models.ImageField(max_length=200, upload_to=image_upload_source)
+    source = models.ImageField(max_length=200, upload_to=freezer_image_upload_source)
     visual = models.URLField(max_length=200, default='')
     create_time = models.DateTimeField('date created', auto_now_add=True, db_index=True)
 
